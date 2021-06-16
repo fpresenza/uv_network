e92acf080b92470c21d189b28bbe15a6181b24d3
Juan Francisco Presenza <francisco.presenza@gmail.com>
2021-06-15 23:09:59 -0300

    wip: kalman

--- ejemplos/kalman_tests/kf_test.py
+++ ejemplos/kalman_tests/kf_test.py
@@ -3,111 +3,72 @@
 """
 @author Francisco Presenza
 @institute LAR - FIUBA, Universidad de Buenos Aires, Argentina
-@date vie nov  6 15:24:28 -03 2020
+@date vie nov 6 15:24:28 -03 2020
 """
 import numpy as np
 import matplotlib.pyplot as plt
 
-import gpsic.plotting.planar as plotting
-from uvnpy.modelos.lineal import integrador
+from uvnpy.model import linear_models
 from uvnpy.filtering import kalman
 
-GPS = wgn = np.random.multivariate_normal
 
-
-def norma(v):
-    sqr_sum = np.multiply(v, v).sum(1)
-    return np.sqrt(sqr_sum)
-
-
-def xBee(p, landmarks, sigma):
-    d = norma(np.subtract(p, landmarks))
+def dist(x, landmarks, sigma):
+    p = x.ravel()
+    d = np.sqrt(np.square(p - landmarks).sum(axis=-1))
     return np.random.normal(d, sigma)
 
 
-class kf_test(kalman.KF):
-    def __init__(self, x, dx, Q, R_gps, R_xbee, ti=0.):
-        super(kf_test, self).__init__(x, dx, ti=0.)
-        self.Id = np.identity(2)
-        self.Q = Q
-        self.R_gps = R_gps
-        self.R_xbee = R_xbee
-        self.dz = None
-
-    def prior(self, dt, u):
-        self._x = self._x + np.multiply(dt, u)
-        Q = self.Q * (dt**2)
-        self._P = self._P + Q
-
-    def modelo_gps(self, z):
-        dz = z - self._x
-        H = self.Id
-        R = self.R_gps
-        return dz, H, R
-
-    def modelo_xbee(self, z, landmarks):
-        diff = np.subtract(self.x, landmarks)
-        hat_z = norma(diff)
-        dz = z - hat_z
-        self.dz = dz
-        H = diff / hat_z.reshape(-1, 1)
-        R = self.R_xbee * np.eye(len(landmarks))
-        return dz, H, R
+def dist_model(x, landmarks, R_dist):
+    p = x.reshape(-1, 2)
+    r = p - landmarks
+    hat_z = np.sqrt(np.square(r).sum(axis=-1)).reshape(-1, 1)
+    H = r / hat_z.reshape(-1, 1)
+    R = R_dist * np.eye(len(landmarks))
+    return hat_z, H, R
 
 
 if __name__ == '__main__':
-    hat_pi = [5., 10.]
-    dpi = [3., 3.]
-    pi = wgn(hat_pi, np.diag(dpi))
+    xi = np.random.uniform(-25, 25, 2)
+    dxi = np.array([3., 3.])
+    Pi = np.diag(dxi**2)
+    hat_xi = np.random.multivariate_normal(xi, Pi)
     Q = 0.5 * np.eye(2)
-    R_gps = 9 * np.eye(2)
-    sigma_xbee = 3.
-    R_xbee = sigma_xbee ** 2
+    R_gps = np.array([[12, 0.], [0, 9.]])
+    sigma_dist = 3.
+    R_dist = sigma_dist ** 2
 
-    landmarks = [(0., 0.), (0., 50.), (50., 0.)]
+    landmarks = np.array([
+        [0., 0.],
+        [0., 50.],
+        [50., 0.]])
 
-    fig = plt.figure()
-    gs = fig.add_gridspec(2, 1)
-    # posición
-    pax = plotting.agregar_ax(
-        gs[0, 0],
-        title='Pos. (verdadero vs. estimado)', title_kw={'fontsize': 11},
-        xlabel='t [seg]', ylabel='posición [m]', label_kw={'fontsize': 10})
+    tiempo = np.arange(0, 1, 0.05)
+    N = len(tiempo)
+    planta = linear_models.random_walk(xi, Q)
+    kf = kalman.KF(hat_xi, Pi)
 
-    iax = plotting.agregar_ax(
-        gs[1, 0],
-        title='Innovación', title_kw={'fontsize': 11},
-        xlabel='t [seg]', ylabel='posición [m]', label_kw={'fontsize': 10})
+    x = np.empty((N, 2))
+    dot_x = np.empty((N, 2))
+    x[0] = planta.x
+    dot_x[0] = planta.dot_x
 
-    for i in range(10):
-        v = integrador(pi)
+    for k, t in enumerate(tiempo[1:]):
+        u = np.array([0.5, 1.])
 
-        kf = kf_test(hat_pi, dpi, Q, R_gps, R_xbee)
-        tiempo = np.arange(0, 50, 0.1)
-        p = [pi]
-        f_p = [hat_pi]
-        f_dz = [np.zeros(3)]
-        for t in tiempo[1:]:
-            u = [0.5, 1.]
+        planta.step(t, u)
 
-            v.step(t, wgn(u, Q))
-            x = v.x
+        kf.prediction(kalman.integrator, t, u.reshape(-1, 1), Q)
 
-            kf.prediccion(t, u)
-            # z_gps = GPS(x, R_gps)
-            # kf.actualizacion(*kf.modelo_gps(z_gps))
-            z_xbee = xBee(x, landmarks, sigma_xbee)
-            kf.actualizacion(*kf.modelo_xbee(z_xbee, landmarks))
-            p.append(x)
-            f_p.append(kf.x)
-            f_dz.append(kf.dz)
+        z_gps = np.random.multivariate_normal(planta.x, R_gps)
+        kf.correction(kalman.gps_model, z_gps.reshape(-1, 1), R_gps)
 
-        t = tiempo
-        p = np.vstack(p)
-        f_p = np.vstack(f_p)
+        z_dist = dist(planta.x, landmarks, sigma_dist)
+        kf.correction(dist_model, z_dist.reshape(-1, 1), landmarks, R_dist)
 
-        plotting.agregar_linea(pax, t, p[:, 0] - f_p[:, 0], color='r')
-        plotting.agregar_linea(pax, t, p[:, 1] - f_p[:, 1], color='g')
-        plotting.agregar_linea(iax, t, f_dz, color='0.5')
+        x[k+1] = planta.x
+        dot_x[k+1] = planta.dot_x
+        kf.save_data()
+
+    kalman.plot(kf.summary(), ground_truth=x[..., None])
 
     plt.show()
--- python/uvnpy/filtering/kalman.py
+++ python/uvnpy/filtering/kalman.py
@@ -4,27 +4,53 @@
 @author: fran
 """
 import numpy as np
-from numpy.linalg import inv
+import collections
+import matplotlib.pyplot as plt
 
 
-def fusionar(v, Fisher):
-    """ Fusion de Kalman.
+Logs = collections.namedtuple('Logs', 't mean cov innovation')
 
-    Fusión de una secuencia de distribuciones gaussianas
-    representadas en su forma canónica
-    (espacio de información de fischer).
 
-    args:
-        v = (v_1, ..., v_n)
-        Fisher = (F_1, ..., F_n)
-    """
-    return np.sum(v, axis=0), np.sum(Fisher, axis=0)
+def integrator(dt, x, u, Q):
+    x = x + u * dt
+    F = np.identity(len(x))
+    return x, F, Q
+
+
+def gps_model(x, R):
+    hat_z = x
+    H = np.identity(len(x))
+    return hat_z, H, R
+
+
+def covariance_prediction(P, F, Q):
+    return F.dot(P).dot(F.T) + Q
+
+
+def covariance_correction(P, H, R):
+    Pz_inv = np.linalg.inv(H.dot(P).dot(H.T) + R)
+    K = P.dot(H.T).dot(Pz_inv)
+    return P - K.dot(H).dot(P)
+
+
+def two_step_covariance(P, F, Q, H, R):
+    P = F.dot(P).dot(F.T) + Q
+    Pz_inv = np.linalg.inv(H.dot(P).dot(H.T) + R)
+    K = P.dot(H.T).dot(Pz_inv)
+    return P - K.dot(H).dot(P)
 
 
 class kalman(object):
-    def __init__(self, xi, dxi, ti=0.):
-        """Filtros de Kalman. """
-        self.iniciar(xi, dxi, ti)
+    def __init__(self, xi, Pi, ti=0.):
+        """Filtros de Kalman"""
+        self.init(xi, Pi, ti)
+        self.logs = Logs(
+            t=[],
+            mean=[],
+            cov=[],
+            innovation={}
+        )
+        self.save_data()
 
     @property
     def x(self):
@@ -34,66 +60,88 @@ class kalman(object):
     def P(self):
         return self._P.copy()
 
-    def iniciar(self, xi, dxi, ti=0., f=None):
+    def init(self, xi, Pi, ti=0.):
         self.t = ti
-        self._x = np.copy(xi)
-        self._P = np.diag(np.square(dxi))
+        self._x = xi.copy().reshape(-1, 1)
+        self._P = Pi.copy()
 
-    def prediccion(self, t, *args):
-        """Paso de predicción
+    def save_data(self):
+        self.logs.t.append(self.t)
+        self.logs.mean.append(self.x)
+        self.logs.cov.append(self.P)
+
+    def summary(self):
+        _innovation = self.logs.innovation.copy()
+        logs = Logs(
+            t=np.array(self.logs.t),
+            mean=np.array(self.logs.mean),
+            cov=np.array(self.logs.cov),
+            innovation=np.empty((len(_innovation), 3), dtype=np.ndarray)
+        )
+        for i, (key, val) in enumerate(_innovation.items()):
+            logs.innovation[i, :2] = key
+            logs.innovation[i, 2] = val
+
+        return logs
+
+    def prediction(self, f, t, *args):
+        """Paso de prediccion
 
         args:
-
             t: tiempo
         """
         dt = t - self.t
         self.t = t
-        self.prior(dt, *args)
+        x, F, Q = f(dt, self._x, *args)
+        self._x = x
+        self._P = F.dot(self._P).dot(F.T) + Q
 
 
 class KF(kalman):
-    def __init__(self, xi, dxi, ti=0.):
-        """Filtro de Kalman en forma clásica. """
-        super(KF, self).__init__(xi, dxi, ti=0.)
+    def __init__(self, xi, Pi, ti=0.):
+        """Filtro de Kalman en forma clasica. """
+        super(KF, self).__init__(xi, Pi, ti)
 
-    def actualizacion(self, dz, H, R):
-        """Paso de corrección
+    def correction(self, h, z, *args):
+        """Paso de correccion
 
         args:
-
-            dz: innovación
-            H: matriz de observación
-            R: covarianza del sensor
+            h: modelo de sensor
+            z: medicion
         """
-        x, P = self._x, self._P
-        Pz = H.dot(P).dot(H.T) + R
-        Pz_inv = inv(Pz)
-        K = P.dot(H.T).dot(Pz_inv)
-        self._x = x + K.dot(dz)
-        self._P = P - K.dot(H).dot(P)
+        hat_z, H, R = h(self._x, *args)
+        dz = z - hat_z
+        Pz = H.dot(self._P).dot(H.T) + R
+        Pz_inv = np.linalg.inv(Pz)
+        K = self._P.dot(H.T).dot(Pz_inv)
+
+        self._x = self._x + K.dot(dz)
+        self._P = self._P - K.dot(H).dot(self._P)
+
+        self.logs.innovation[self.t, h.__name__] = dz.copy()
 
 
 class KFi(kalman):
-    def __init__(self, xi, dxi, ti=0.):
+    def __init__(self, xi, Pi, ti=0.):
         """Filtro de Kalman en forma alternativa. """
-        super(KFi, self).__init__(xi, dxi, ti=0.)
+        super(KFi, self).__init__(xi, Pi, ti)
 
-    def actualizacion(self, dy, Y):
-        """Paso de corrección
+    def correction(self, dy, Y):
+        """Paso de correccion
 
         args:
 
-            dy: innovacón en espacio de información
-            Y: matriz de innovación
+            dy: innovacón en espacio de informacion
+            Y: matriz de innovacion
         """
         x, P = self._x, self._P
-        I_prior = inv(P)
-        self._P = inv(I_prior + Y)
+        I_prior = np.linalg.inv(P)
+        self._P = np.linalg.inv(I_prior + Y)
         self._x = x + P.dot(dy)
 
 
 class KCFE(kalman):
-    def __init__(self, xi, dxi, ti=0.):
+    def __init__(self, xi, Pi, ti=0.):
         """Filtro de Kalman por Consenso en Estimaciones
 
         Ver:
@@ -102,16 +150,16 @@ class KCFE(kalman):
               Stability and Performance'',
             IEEE Conference on Decision and Control (2009).
         """
-        super(KCFE, self).__init__(xi, dxi, ti=0.)
+        super(KCFE, self).__init__(xi, Pi, ti)
         self.t_a = ti
 
-    def actualizacion(self, t, dy, Y, x_j, c=30.):
-        """Paso de corrección
+    def correction(self, t, dy, Y, x_j, c=30.):
+        """Paso de correccion
 
         args:
 
             t: tiempo
-            dy: innovacion en espacio de información
+            dy: innovacion en espacio de informacion
             Y: matriz de innovacion
             x_j: tupla de estimados
         """
@@ -119,8 +167,8 @@ class KCFE(kalman):
         self.t_a = t
         x, P = self._x, self._P
 
-        I_prior = inv(P)
-        P = inv(I_prior + Y)
+        I_prior = np.linalg.inv(P)
+        P = np.linalg.inv(I_prior + Y)
 
         d_i = len(x_j)
         suma = np.sum(x_j, axis=0) - d_i * x
@@ -132,9 +180,10 @@ class KCFE(kalman):
 
 
 class IF(object):
-    def __init__(self, xi, dxi, ti=0.):
-        """Filtro de Informacion. """
-        self.iniciar(xi, dxi, ti)
+    def __init__(self, xi, Pi, ti=0.):
+        """Filtro de Informacion."""
+        self.init(xi, Pi, ti)
+        self.motion_model = None
 
     @property
     def v(self):
@@ -144,17 +193,17 @@ class IF(object):
     def Fisher(self):
         return self._I.copy()
 
-    def iniciar(self, xi, dxi, ti=0., f=None):
+    def init(self, xi, Pi, ti=0., f=None):
         self.t = ti
-        self._I = np.diag(1./np.square(dxi))
+        self._I = np.linalg.inv(Pi)
         self._v = self._I.dot(xi)
 
-    def transformar(self, u, M):
-        Minv = inv(M)
+    def transform(self, u, M):
+        Minv = np.linalg.inv(M)
         v = Minv.dot(u)
         return v, Minv
 
-    def prediccion(self, t, *args):
+    def prediction(self, t, *args):
         """Paso de predicción
 
         args:
@@ -163,17 +212,113 @@ class IF(object):
         """
         dt = t - self.t
         self.t = t
-        x, P = self.transformar(self._v, self._I)
-        x, P = self.prior(dt, x, P, *args)
-        self._v, self._I = self.transformar(x, P)
+        x, P = self.transform(self._v, self._I)
+        x, F, Q = self.motion_model(dt, self._x, *args)
+        P = F.dot(P).dot(F.T) + Q
+        self._v, self._I = self.transform(x, P)
 
-    def actualizacion(self, y, Y):
-        """Paso de corrección
+    def correction(self, y, Y):
+        """Paso de correccion
 
         args:
 
-            y: contribuciones en espacio de información
+            y: contribuciones en espacio de informacion
             Y: matriz de contribución
         """
         self._v = self._v + y
         self._I = self._I + Y
+
+
+def plot(logs, ground_truth=None, basis=None):
+    t = logs.t
+    mean = logs.mean
+    cov = logs.cov
+    innovation = logs.innovation
+    gt = ground_truth
+    if basis is not None:
+        m = basis.shape[-1]
+        B = basis
+        BT = B.swapaxes(-1, -2)
+        mean = np.matmul(BT, mean).reshape(-1, m)
+        cov = np.matmul(BT, np.matmul(cov, B))
+    else:
+        mean = mean.reshape(len(t), -1)
+
+    figs = []
+
+    f, axes = plt.subplots(2, 2)
+    f.suptitle('Metrics')
+    figs.append(f)
+
+    lines = axes[0, 0].plot(t, mean)
+    axes[0, 0].set_title('Estimate')
+    axes[0, 0].grid(1)
+    axes[0, 0].minorticks_on()
+    axes[0, 0].set_xlabel(r'$t\,[sec]$')
+    axes[0, 0].set_ylabel(r'$x(t)$')
+
+    if gt is not None:
+        if basis is not None:
+            gt = np.matmul(BT, gt).reshape(-1, m)
+        gt = gt.reshape(len(t), -1)
+
+        # plt.gca().set_prop_cycle(None)
+        for i, l in enumerate(lines):
+            axes[0, 0].plot(t, gt[:, i], color=l.get_color(), ls='dotted')
+
+        axes[1, 0].set_title('Error')
+        axes[1, 0].plot(t, gt - mean)
+        axes[1, 0].grid(1)
+        axes[1, 0].minorticks_on()
+        axes[1, 0].set_xlabel(r'$t\,[sec]$')
+        axes[1, 0].set_ylabel(r'$x(t) - \hat{x}(t)$')
+
+        sigma = np.sqrt(cov.diagonal(axis1=1, axis2=2))
+        plt.gca().set_prop_cycle(None)
+        axes[1, 0].plot(t, sigma, ls='dashed')
+        plt.gca().set_prop_cycle(None)
+        axes[1, 0].plot(t, -sigma, ls='dashed')
+
+    axes[0, 1].set_title('Covariance eigenvalues')
+    axes[0, 1].plot(t, np.sqrt(np.linalg.eigvalsh(cov)))
+    axes[0, 1].grid(1)
+    axes[0, 1].minorticks_on()
+    axes[0, 1].set_xlabel(r'$t\,[sec]$')
+    axes[0, 1].set_ylabel(r'$\sqrt{\lambda(P)(t)}$')
+
+    axes[1, 1].set_title('Covariance metrics')
+    axes[1, 1].plot(t, cov.trace(axis1=1, axis2=2), label=r'$\rm{tr}(P)$')
+    axes[1, 1].plot(t, np.log(np.linalg.det(cov)), label=r'$\rm{logdet}(P)$')
+    axes[1, 1].grid(1)
+    axes[1, 1].minorticks_on()
+    axes[1, 1].set_xlabel(r'$t\,[sec]$')
+    axes[1, 1].legend()
+
+    sensors = np.unique(innovation[:, 1])
+    s = len(sensors)
+    f, axes = plt.subplots(s, 2)
+    axes = axes.reshape(s, 2)
+    axes[0, 0].set_title('Measurements')
+    axes[0, 1].set_title('Auto-Correlation')
+    figs.append(f)
+
+    for i, s in enumerate(sensors):
+
+        match = innovation[:, 1] == s
+        t = innovation[match, 0]
+        dz = np.array(innovation[match, 2].tolist())
+        dz = dz.reshape(len(t), -1)
+        axes[i, 0].plot(t, dz, ls='', marker='.', markersize=2)
+        axes[i, 0].grid(1)
+        axes[i, 0].minorticks_on()
+        axes[i, 0].set_xlabel(r'$t\,[sec]$')
+        axes[i, 0].set_ylabel(r'$\delta z$')
+        axes[i, 0].set_xlim(logs.t[0], None)
+
+        for dz_i in dz.T:
+            axes[i, 1].acorr(dz_i, usevlines=False, linestyle='-')
+        axes[i, 1].grid(1)
+        axes[i, 1].minorticks_on()
+        axes[i, 1].set_xlabel(r'$\Delta t\,[sec]$')
+
+    return figs
