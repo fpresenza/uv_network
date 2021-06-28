ba8734bfee449c4bd59769819e36979135ad6cb8
Juan Francisco Presenza <francisco.presenza@gmail.com>
2021-06-28 20:53:26 -0300

    +cost functions

--- python/uvnpy/control/cost_functions.py
+++ python/uvnpy/control/cost_functions.py
@@ -9,7 +9,7 @@ import numpy as np
 
 
 def collision(p):
-    """Costo de repulsión entre agentes puntuales.
+    """Costo de repulsion entre agentes puntuales.
 
     El costo es igual a la suma de los cuadrados de la
     inversa de las distancias entre agentes.
@@ -18,3 +18,32 @@ def collision(p):
     d2 = np.square(r).sum(axis=-1)
     d2[d2 != 0] **= -1
     return d2.sum()
+
+
+def intercollision(p, q):
+    """Costo de repulsion entre dos grupos de agentes puntuales.
+
+    El costo es igual a la suma de los cuadrados de la
+    inversa de las distancias entre agentes.
+    """
+    r = p[..., None, :] - q[..., None, :, :]
+    d2 = np.square(r).sum(axis=-1)
+    d2[d2 != 0] **= -1
+    return d2.sum()
+
+
+def maxdist(p):
+    """Maxima distancia entre agentes puntuales."""
+    r = p[..., None, :] - p[..., None, :, :]
+    d2 = np.square(r).sum(axis=-1)
+    dmax = np.sqrt(d2.max((-2, -1)))
+    return dmax
+
+
+def mindist(p):
+    """Maxima distancia entre agentes puntuales."""
+    r = p[..., None, :] - p[..., None, :, :]
+    d2 = np.square(r).sum(axis=-1)
+    ii = np.eye(d2.shape[-1], dtype=bool)
+    dmin = np.sqrt(d2[..., ~ii].min(axis=-1))
+    return dmin
