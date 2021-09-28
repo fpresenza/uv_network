d713a1cda34ac45f76e2459a85212b135ffc414f
Juan Francisco Presenza <francisco.presenza@gmail.com>
2021-09-28 11:38:06 -0300

    import subpackages

--- python/uvnpy/__init__.py
+++ python/uvnpy/__init__.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from . import control    # noqa
+from . import filtering  # noqa
+from . import model      # noqa
+from . import network    # noqa
+from . import rsn        # noqa
+from . import toolkit    # noqa
