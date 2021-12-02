90bfc33be1f36a5961cd1a0757f008829aa992e6
Juan Francisco Presenza <francisco.presenza@gmail.com>
2021-12-02 12:13:45 -0300

    rsn/control/rigidity_maintenance cambios menores

--- python/uvnpy/rsn/control.py
+++ python/uvnpy/rsn/control.py
@@ -12,27 +12,27 @@ from uvnpy.toolkit import functions
 from uvnpy.toolkit.calculus import derivative_eval
 
 
-class rigidity_maintenance(object):
-    def __init__(self, dof, dmax, steepness, exponent):
+class centralized_rigidity_maintenance(object):
+    def __init__(self, dim, dmax, steepness, exponent):
         """Control gradiente descendiente.
 
         args:
-        dof: degrees of freedom vehiculos
-        dmax: distancia maxima de conexion
-        steepness: factor de decaimiento de intensidad de señal
-        exponent: exponente (>0) al que se eleva el autovalor de rigidez
+            dim: dimension del espacio (2 o 3)
+            dmax: distancia maxima de conexion
+            steepness: factor de decaimiento de intensidad de señal
+            exponent: exponente (>0) al que se eleva el autovalor de rigidez
         """
-        self.dof = dof
+        self.dim = dim
         self.midpoint = dmax
         self.steepness = steepness
         self.r = exponent
-        self.freedom = int(dof * (dof + 1)/2)
+        self.dof = int(dim * (dim + 1)/2)
 
     def gradient(self, x, eigenvalue, eigenvector):
-        dL_dx = derivative_eval(self.weighted_rigidity_matrix, x)
-        eigengrad = eigenvector.dot(dL_dx).dot(eigenvector)
-        eigengrad = eigengrad.reshape(x.shape)
-        u = -self.r * eigenvalue**(-self.r - 1) * eigengrad
+        dS_dx = derivative_eval(self.weighted_rigidity_matrix, x)
+        dlambda_dx = eigenvector.dot(dS_dx).dot(eigenvector)
+        dlambda_dx = dlambda_dx.reshape(x.shape)
+        u = -self.r * eigenvalue**(-self.r - 1) * dlambda_dx
         return u
 
     def weighted_rigidity_matrix(self, x):
@@ -44,5 +44,5 @@ class rigidity_maintenance(object):
     def update(self, x):
         S = self.weighted_rigidity_matrix(x)
         e, V = np.linalg.eigh(S)
-        grad = self.gradient(x, e[self.freedom], V[:, self.freedom])
+        grad = self.gradient(x, e[self.dof], V[:, self.dof])
         return -grad
