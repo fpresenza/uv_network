dd19c43a5e6c436cb438de823c7f7345fa86d75e
Juan Francisco Presenza <francisco.presenza@gmail.com>
2021-12-16 19:23:00 -0300

    init aunotonomous_agents

--- python/uvnpy/__init__.py
+++ python/uvnpy/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from . import control    # noqa
-from . import filtering  # noqa
-from . import model      # noqa
-from . import network    # noqa
-from . import rsn        # noqa
-from . import toolkit    # noqa
+from . import control              # noqa
+from . import filtering            # noqa
+from . import model                # noqa
+from . import network              # noqa
+from . import rsn                  # noqa
+from . import toolkit              # noqa
+from . import autonomous_agents    # noqa
