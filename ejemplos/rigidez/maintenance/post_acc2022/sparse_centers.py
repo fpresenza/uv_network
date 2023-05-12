9c56966bde5c8dbeac11786f40f62c3ea935e684
fpresenza <francisco.presenza@gmail.com>
2023-05-12 13:07:51 -0300

    post_acc2022: sparse_centers wip

--- ejemplos/rigidez/maintenance/post_acc2022/sparse_centers.py
+++ ejemplos/rigidez/maintenance/post_acc2022/sparse_centers.py
@@ -4,23 +4,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from uvnpy.network import subsets, disk_graph, plot
-from uvnpy.rsn import rigidity
+from uvnpy.network.subsets import degree_load_std, degree_load_flat, multihop_subframework
+from uvnpy.network import plot
+from uvnpy.rsn.rigidity import extents, minimum_radius, subframework_based_rigidity
+from uvnpy.rsn.distances import matrix_between as distance_between
+from uvnpy.network.disk_graph import adjacency as disk_adjacency
 
-check_rigidity = rigidity.subframework_based_rigidity
 
-# metric = subsets.degree_load_std
-# metric = subsets.degree_load_flat
-# metric = lambda A, h : subsets.degree_load_std(A, h) + 60*np.max(h)
+def generate_position(n, xlim, ylim, radius):
+    """Genera una posicion donde los nodos estan a mas
+    de cierto radio de separacion"""
+    xl, xh = xlim
+    yl, yh = ylim
+    p = np.random.uniform((xl, yl), (xh, yh), 2)
+    for i in range(1, n):
+        found = False
+        while not found:
+            q = np.random.uniform((xl, yl), (xh, yh), 2)
+            dist = distance_between(p, q)
+            if np.all(dist > radius):
+                found = True
+                p = np.vstack([p, q])
+
+    return p
+
+
+# metric = degree_load_std
+# metric = degree_load_flat
+# metric = lambda A, h : degree_load_std(A, h) + 60*np.max(h)
 # metric = lambda A, h : np.sum(h * h)
 metrics = [
-    (subsets.degree_load_std, r'$\mathcal{L}(h)$'),
-    (subsets.degree_load_flat, r'$\mathcal{L}_{flat}(h)$'),
+    (degree_load_std, r'$\mathcal{L}(h)$'),
+    # (degree_load_flat, r'$\mathcal{L}_{flat}(h)$'),
     (lambda A, h: np.max(h), r'$\max_i \; h_i$')
 ]
 
 
-# x = np.array([
+# p = np.array([
 #     [-0.8967,  0.4359],
 #     [-0.1932, -0.2075],
 #     [ 0.6964,  0.9842],
@@ -44,96 +64,132 @@ metrics = [
 #     [0., 1., 0., 1., 1., 1., 0., 0., 0., 0.],
 #     [0., 0., 1., 0., 1., 0., 1., 0., 0., 0.]])
 
-n, d = 10, 2
-found_rigid = False
-while not found_rigid:
-    x = np.random.uniform(-1, 1, (n, d))
-    A = disk_graph.adjacency(x, 0.8)
-    found_rigid = rigidity.algebraic_condition(A, x)
+# n, d = 10, 2
+# found_rigid = False
+# while not found_rigid:
+#     p = np.random.uniform(-1, 1, (n, d))
+#     A = disk_adjacency(p, 0.8)
+#     found_rigid = rigidity.algebraic_condition(A, p)
 
-print(A, x)
+# print(A, p)
+
+# A = np.array([
+#     [0., 1., 1., 0., 0., 0., 0., 0., 1., 1.],
+#     [1., 0., 1., 0., 0., 0., 0., 0., 0., 0.],
+#     [1., 1., 0., 1., 0., 1., 0., 0., 0., 0.],
+#     [0., 0., 1., 0., 1., 1., 0., 0., 0., 0.],
+#     [0., 0., 0., 1., 0., 1., 1., 0., 0., 0.],
+#     [0., 0., 1., 1., 1., 0., 1., 1., 1., 0.],
+#     [0., 0., 0., 0., 1., 1., 0., 1., 0., 0.],
+#     [0., 0., 0., 0., 0., 1., 1., 0., 1., 0.],
+#     [1., 0., 0., 0., 0., 1., 0., 1., 0., 1.],
+#     [1., 0., 0., 0., 0., 0., 0., 0., 1., 0.]])
+
+# p = np.array([
+#     [ 2.921961,  0.962442],
+#     [ 3.479602,  1.951268],
+#     [ 2.496763,  2.044489],
+#     [-0.05499 ,  1.955132],
+#     [-1.120506,  0.903233],
+#     [ 0.856702,  0.878715],
+#     [-0.533504, -0.472975],
+#     [ 0.975142, -0.536853],
+#     [ 1.392686,  0.203473],
+#     [ 2.188298,  0.004104]])
 
 A = np.array([
-    [0., 1., 1., 0., 0., 0., 0., 0., 1., 1.],
-    [1., 0., 1., 0., 0., 0., 0., 0., 0., 0.],
-    [1., 1., 0., 1., 0., 1., 0., 0., 0., 0.],
-    [0., 0., 1., 0., 1., 1., 0., 0., 0., 0.],
-    [0., 0., 0., 1., 0., 1., 1., 0., 0., 0.],
-    [0., 0., 1., 1., 1., 0., 1., 1., 1., 0.],
-    [0., 0., 0., 0., 1., 1., 0., 1., 0., 0.],
-    [0., 0., 0., 0., 0., 1., 1., 0., 1., 0.],
-    [1., 0., 0., 0., 0., 1., 0., 1., 0., 1.],
-    [1., 0., 0., 0., 0., 0., 0., 0., 1., 0.]])
+    [0., 1., 1., 1., 1., 1., 1.],
+    [1., 0., 1., 1., 0., 0., 0.],
+    [1., 1., 0., 1., 0., 0., 0.],
+    [1., 1., 1., 0., 0., 0., 1.],
+    [1., 0., 0., 0., 0., 1., 1.],
+    [1., 0., 0., 0., 1., 0., 1.],
+    [1., 0., 0., 1., 1., 1., 0.]])
 
-x = np.array([
-    [ 2.921961,  0.962442],
-    [ 3.479602,  1.951268],
-    [ 2.496763,  2.044489],
-    [-0.05499 ,  1.955132],
-    [-1.120506,  0.903233],
-    [ 0.856702,  0.878715],
-    [-0.533504, -0.472975],
-    [ 0.975142, -0.536853],
-    [ 1.392686,  0.203473],
-    [ 2.188298,  0.004104]])
+p = np.array([
+    [0.5 , 0.  ],
+    [1.  , 0.  ],
+    [0.75, 0.25],
+    [0.75, 0.5 ],
+    [0.  , 0.  ],
+    [0.25, 0.25],
+    [0.25, 0.5 ]])
 
 
-for metric, name in metrics:
-    extents = rigidity.extents(A, x)
-    print('----', name, '----')
-    print(extents, metric(A, extents))
+for k in range(1):
+    print(k)
+    threshold = 1e-4
+    # n = np.random.randint(10, 20)
+    # p = generate_position(n, (0, 1), (0, 1), 0.02)
+    # A0 = disk_adjacency(p, dmax=2/np.sqrt(n))
+    # A = minimum_radius(A0, p, threshold)
 
-    for h in reversed(np.unique(extents)):
-        max_extent = np.where(extents == h)[0]
-        repeat = True
-        while repeat:
-            min_load = np.inf
-            remove = None
-            for i in max_extent:
-                sparsed = extents.copy()
-                sparsed[i] = 0
-                if check_rigidity(A, x, sparsed) is True:
-                    new_load = metric(A, sparsed)
-                    if new_load < min_load:
-                        min_load = new_load
-                        remove = i
+    for m, (metric, name) in enumerate(metrics):
+        hops = extents(A, p, threshold)
+        # print('----', name, '----')
+        # print(hops, metric(A, hops))
 
-            if min_load < np.inf:
-                extents[remove] = 0
-                max_extent = np.delete(max_extent, max_extent == remove)
-            else:
-                repeat = False
+        for h in reversed(np.unique(hops)):
+            max_extent = np.where(hops == h)[0]
+            repeat = True
+            while repeat:
+                min_load = np.inf
+                remove = None
+                for i in max_extent:
+                    sparsed = hops.copy()
+                    sparsed[i] = 0
+                    if subframework_based_rigidity(A, p, sparsed, threshold) is True:
+                        new_load = metric(A, sparsed)
+                        if new_load < min_load:
+                            min_load = new_load
+                            remove = i
 
-    print(extents, metric(A, extents))
-    centers = np.where(extents > 0)[0]
+                if min_load < np.inf:
+                    hops[remove] = 0
+                    max_extent = np.delete(max_extent, max_extent == remove)
+                else:
+                    repeat = False
 
-    if len(centers) == 1:
-        is_rigid = check_rigidity(A, x, extents, return_binding=True)
-    else:
-        is_rigid, B = check_rigidity(A, x, extents, return_binding=True)
+        # print(hops, metric(A, hops))
+        centers = np.where(hops > 0)[0]
 
-    if not is_rigid:
-        print('algo salio mal.')
+        if len(centers) == 1:
+            is_rigid = subframework_based_rigidity(A, p, hops, threshold)
+        else:
+            is_rigid, B = subframework_based_rigidity(
+                A, p, hops, threshold, return_binding=True)
 
-    fig, ax = plt.subplots()
-    fig.suptitle(name)
-    ax.grid(1)
-    ax.set_aspect('equal')
-    ax.set_xlim(x[:, 0].min() * 1.15, x[:, 0].max() * 1.15)
-    ax.set_ylim(x[:, 1].min() * 1.15, x[:, 1].max() * 1.15)
+        if not is_rigid:
+            print('algo salio mal.')
 
-    for i in range(len(x)):
-        ax.text(x[i, 0] + 0.02, x[i, 1] + 0.02, '{}'.format(i))
-    for i, c in enumerate(centers):
-        Ac, xc = subsets.multihop_subframework(A, x, c, extents[c])
-        plot.graph(ax, xc, Ac, color='C{}'.format(i))
-        ax.scatter(
-            x[c, 0], x[c, 1], color='C{}'.format(i), facecolor='none', s=100)
+        fig, ax = plt.subplots()
+        fig.suptitle(name)
+        ax.grid(1)
+        ax.set_aspect('equal')
+        ax.set_xlim(0, 1)
+        ax.set_ylim(0, 1)
 
-    if len(centers) > 1:
-        binding = np.nonzero(np.sum(B, axis=1))[0]
-        plot.nodes(ax, x[binding], color='k', marker='s', s=60)
-        plot.edges(
-            ax, x[binding], B[binding][:, binding], color='k', ls='--', lw=0.5)
+        plot.edges(ax, p, A, color='k', lw=0.4)
 
-plt.show()
+        for i in range(len(p)):
+            ax.text(
+                p[i, 0] + 0.01, p[i, 1] + 0.01, '{}'.format(i), size=5)
+        for i, c in enumerate(centers):
+            Ac, pc = multihop_subframework(A, p, c, hops[c])
+            plot.graph(ax, pc, Ac, color='C{}'.format(i))
+            ax.scatter(
+                p[c, 0], p[c, 1],
+                color='C{}'.format(i), s=120, label=r'$h={}$'.format(hops[c]))
+        ax.legend()
+
+        if len(centers) > 1:
+            binding = np.nonzero(np.sum(B, axis=1))[0]
+            plot.nodes(ax, p[binding], color='k', marker='s', s=60)
+            plot.edges(
+                ax, p[binding], B[binding][:, binding],
+                color='k', ls='--', lw=0.5)
+
+        fig.savefig(
+            '/tmp/sparse_centers{}{}.png'.format(k, m), format='png', dpi=360)
+
+# plt.show()
