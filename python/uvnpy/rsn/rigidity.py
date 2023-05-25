35b0cebe65a4e8e0ef310bddfbca8040e1b6bbd4
fpresenza <francisco.presenza@gmail.com>
2023-05-25 00:52:10 -0300

    rsn/rigidity: wip sparse centers and rigidly linked

--- python/uvnpy/rsn/rigidity.py
+++ python/uvnpy/rsn/rigidity.py
@@ -295,11 +295,8 @@ def minimum_radius(A, p, threshold=1e-4, return_radius=False):
 
 
 def rigidly_linked(A, p, extents, threshold=1e-4):
-    """Determines whether a set of subframeworks is rigidly linked.
-    If one of them is the whole framework, condition is satisfied.
-    If their union does not equal the whole framework, condition is not
-    satisfied.
-
+    """Determines whether a set of subframeworks cover the whole framework and
+    is rigidly linked
 
     Returns:
         Adjacency matrix of the link graph
@@ -310,45 +307,92 @@ def rigidly_linked(A, p, extents, threshold=1e-4):
     n, d = p.shape
     geo = geodesics(A)
 
-    # check if any subframework covers the whole framework
-    centers = extents > 0
-    proper = extents < geo.max(axis=0)
-    proper_centers = np.logical_and(proper, centers)
+    # computes the union between subgraphs
+    centers = np.nonzero(extents)[0]
+    Au = np.zeros((n, n))
+    is_in_count = np.zeros(n, dtype=int)
+    subsets = {}
+    for i in centers:
+        is_in = np.where(geo[i] <= extents[i])[0]    # inside subgraph
+        is_in_count[is_in] += 1
+        subsets[i] = is_in
+        idx = np.ix_(is_in, is_in)
+        Au[idx] = A[idx].copy()
 
-    # check if subframeworks cover all vertices
-    is_in = geo[centers] <= extents[centers].reshape(-1, 1)
-    covered = np.any(is_in, axis=0)
-    if not np.all(covered):
+    # check if subgraphs cover all vertices
+    degree = np.sum(Au, axis=0)
+    uncovered = degree == 0
+    if np.any(uncovered):
         raise ValueError(
-            'Nodes {} not covered.'.format(np.where(~covered)[0]))
+            'Node(s) {} not covered.'.format(np.where(uncovered)[0]))
 
+    # link graph
+    Al = A - Au
     # detect link nodes
-    L = np.array([], dtype=int)
-    AL = A.copy()
-    for i in np.where(proper_centers)[0]:
-        per = geo[i] == extents[i]            # periferic
-        out = geo[i] > extents[i]             # outside subframework
-        ext = np.any(A[per][:, out], axis=1)  # connected to external nodes
-        links = np.argwhere(per)[ext]         # link nodes
-        if len(links) < d:
+    shared_nodes = np.where(is_in_count > 1)[0]
+    link_edges = np.argwhere(np.triu(Al))
+    link_nodes = np.union1d(shared_nodes, link_edges)
+    for i in centers:
+        link_subset = np.intersect1d(link_nodes, subsets[i])
+        num_link = len(link_subset)
+        if num_link < d:
             raise ValueError(
                 'Subframework {} hasn\'t got enough link nodes.'.format(i))
-        AL[links.ravel(), links] = 1 - np.eye(len(links))
-        L = np.union1d(L, links)
-
-    # remove all non-linking edges
-    NL = np.setdiff1d(range(n), L)
-    AL[NL] = AL[:, NL] = 0
+        Al[np.ix_(link_subset, link_subset)] = 1 - np.eye(num_link)
 
     # check if the link graph is rigid
-    if np.any(proper_centers):
-        if not algebraic_condition(AL[L][:, L], p[L], threshold):
-            raise ValueError('Link framework is flexible.')
+    if not algebraic_condition(
+            Al[np.ix_(link_nodes, link_nodes)], p[link_nodes], threshold):
+        raise ValueError('Link framework is flexible.')
 
-    return AL
+    return Al
 
 
-def sparse_centers(A, p, extents, metric, threshold=1e-4):
+def rigidly_linked_by_vertices(A, p, extents, threshold=1e-4):
+    """Determines whether a set of subframeworks cover the whole framework and
+    is rigidly linked
+
+    Returns:
+        Adjacency matrix of the link graph
+
+    Raise:
+        ValueError: whenever condition is not satisfied
+    """
+    n, d = p.shape
+    geo = geodesics(A)
+
+    centers = np.nonzero(extents)[0]
+    c = len(centers)
+    uncovered = geo[centers] > extents[centers].reshape(-1, 1)
+    uncovered_sum = np.sum(uncovered, axis=0)
+    isolated = uncovered_sum == c
+    if np.any(isolated):
+        raise ValueError(
+            'Node(s) {} not covered.'.format(np.where(isolated)[0]))
+
+    # link graph
+    # detect link nodes
+    link_nodes = uncovered_sum < c - 1
+    Al = np.zeros((n, n))
+    for i in centers:
+        subset = geo[i] <= extents[i]
+        link_subset = np.logical_and(link_nodes, subset)
+        num_link = sum(link_subset)
+        if num_link < d:
+            raise ValueError(
+                'Subframework {} hasn\'t got enough link nodes.'.format(i))
+        Al[np.ix_(link_subset, link_subset)] = 1 - np.eye(num_link)
+
+    # check if the link graph is rigid
+    if not algebraic_condition(
+            Al[np.ix_(link_nodes, link_nodes)], p[link_nodes], threshold):
+        raise ValueError('Link framework is flexible.')
+
+    return Al
+
+
+def sparse_centers_full_search(
+        A, p, extents, metric, threshold=1e-4, vertices_only=False):
     """Dada un conjunto de extensiones, elimina subframeworks iterativamente
     hasta que no puede eliminar mas dado que se pierde rigidez o no se cubre
     todo el framework.
@@ -356,18 +400,102 @@ def sparse_centers(A, p, extents, metric, threshold=1e-4):
     Requires:
         framework is rigid
     """
+    import itertools
+
+    if vertices_only:
+        is_linked = rigidly_linked_by_vertices
+    else:
+        is_linked = rigidly_linked
+
+    n = len(p)
+    geo = geodesics(A)
+    max_hops = extents.max()
+    search_space = itertools.product(range(max_hops + 1), repeat=n)
+
+    min_value = np.inf
+    for h in search_space:
+        h = np.array(h)
+        try:
+            is_linked(A, p, h, threshold)
+            for i in np.nonzero(h)[0]:
+                Vi = geo[i] <= h[i]
+                Ai = A[Vi][:, Vi]
+                xi = p[Vi]
+                if not algebraic_condition(Ai, xi, threshold):
+                    raise ValueError
+            new_value = metric(A, h)
+            if new_value < min_value:
+                min_value = new_value
+                h_opt = h
+        except ValueError:
+            pass
+
+    return h_opt
+
+
+def sparse_centers_binary_search(
+        A, p, extents, metric, threshold=1e-4, vertices_only=False):
+    """Dada un conjunto de extensiones, elimina subframeworks iterativamente
+    hasta que no puede eliminar mas dado que se pierde rigidez o no se cubre
+    todo el framework.
+
+    Requires:
+        framework is rigid
+    """
+    import itertools
+
+    if vertices_only:
+        is_linked = rigidly_linked_by_vertices
+    else:
+        is_linked = rigidly_linked
+
+    n = len(p)
+    search_factors = itertools.product((0, 1), repeat=n)
+
+    min_value = np.inf
+    for f in search_factors:
+        h = np.multiply(extents, f)
+        try:
+            is_linked(A, p, h, threshold)
+            new_value = metric(A, h)
+            if new_value < min_value:
+                min_value = new_value
+                h_opt = h
+        except ValueError:
+            pass
+
+    return h_opt
+
+
+def sparse_centers(A, p, extents, metric, threshold=1e-4, vertices_only=False):
+    """Dada un conjunto de extensiones, elimina subframeworks iterativamente
+    hasta que no puede eliminar mas dado que se pierde rigidez o no se cubre
+    todo el framework.
+
+    Requires:
+        framework is rigid
+    """
+    if vertices_only:
+        is_linked = rigidly_linked_by_vertices
+    else:
+        is_linked = rigidly_linked
+
     n = len(p)
     hops = extents.copy()
     centers = np.arange(n)
     min_found = False
+    count = 0
     while not min_found:
         min_value = np.inf
         remove = None
+        print('----')
         for i in centers:
+            count += 1
+            print(count)
             sparsed = hops.copy()
             sparsed[i] = 0
             try:
-                rigidly_linked(A, p, sparsed, threshold)
+                is_linked(A, p, sparsed, threshold)
                 new_value = metric(A, sparsed)
                 if new_value < min_value:
                     min_value = new_value
@@ -380,4 +508,45 @@ def sparse_centers(A, p, extents, metric, threshold=1e-4):
             centers = np.delete(centers, centers == remove)
         else:
             min_found = True
+    return hops
+
+
+def sparse_centers_two_steps(
+        A, p, extents, metric, threshold=1e-4, vertices_only=False):
+    """Dada un conjunto de extensiones, elimina subframeworks iterativamente
+    hasta que no puede eliminar mas dado que se pierde rigidez o no se cubre
+    todo el framework.
+
+    Requires:
+        framework is rigid
+    """
+    if vertices_only:
+        is_linked = rigidly_linked_by_vertices
+    else:
+        is_linked = rigidly_linked
+
+    hops = extents.copy()
+    for h in reversed(np.unique(hops)):
+        max_extent = np.where(hops == h)[0]
+        min_found = False
+        while not min_found:
+            min_value = np.inf
+            remove = None
+            for i in max_extent:
+                sparsed = hops.copy()
+                sparsed[i] = 0
+                try:
+                    is_linked(A, p, sparsed, threshold)
+                    new_value = metric(A, sparsed)
+                    if new_value < min_value:
+                        min_value = new_value
+                        remove = i
+                except ValueError:
+                    pass
+
+            if min_value < np.inf:
+                hops[remove] = 0
+                max_extent = np.delete(max_extent, max_extent == remove)
+            else:
+                min_found = True
     return hops
