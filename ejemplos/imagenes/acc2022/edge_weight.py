0e39c2144410b0acf6c896ba31fcb840c9605762
fpresenza <francisco.presenza@gmail.com>
2023-05-29 17:36:59 -0300

    imagenes/acc2022: edge wights wip

--- ejemplos/imagenes/acc2022/edge_weight.py
+++ ejemplos/imagenes/acc2022/edge_weight.py
@@ -8,22 +8,38 @@ import matplotlib.pyplot as plt
 
 from uvnpy.toolkit.functions import logistic
 
-fig, ax = plt.subplots(figsize=(3.5, 1.25))
+
+plt.rcParams['text.usetex'] = False
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
+plt.rcParams['mathtext.fontset'] = 'dejavuserif'
+plt.rcParams['font.family'] = 'serif'
+
+fig, ax = plt.subplots(figsize=(3.5, 2.))
 fig.subplots_adjust(left=0.14, bottom=0.32)
 ax.grid(1)
 ax.minorticks_on()
+ax.tick_params(
+    axis='both',       # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    pad=1,
+    labelsize='small')
 
 d = np.linspace(0, 2, 200)
-w = logistic(d, steepness=40, midpoint=0.8)
+print(d[::50])
+w = logistic(d, steepness=40, midpoint=1)
 
 ax.plot(d, w)
 ax.vlines(1, 0, 1, color='k', alpha=0.6, ls='--', lw=0.5)
 ax.axvspan(0, 1, color='green', alpha=0.2)
 ax.axvspan(1, 2, color='red', alpha=0.2)
 ax.set_xlim(0, 2)
-ax.set_xlabel(r'$d_{{ij}} / d_{{\max}}$', fontsize=7)
-ax.set_ylabel(r'$w_{{ij}}$', fontsize=7)
-ax.tick_params(axis='both', which='major', labelsize=6)
+ax.set_xlabel(r'$\Vert p_i - p_j \Vert \; / \; \rho$', fontsize=10)
+ax.set_ylabel(r'$w(i, j)}$', fontsize=10)
+ax.set_xticklabels([0, 0.5, 1, 1.5, 2])
+ax.set_xticks([0, 0.5, 1, 1.5, 2])
+ax.set_yticklabels([0, 0.5, 1])
+ax.set_yticks([0, 0.5, 1])
 plt.show()
 
-fig.savefig('/tmp/edge_weight.pdf', format='pdf')
+fig.savefig('/tmp/edge_weight.png', format='png', dpi=360)
