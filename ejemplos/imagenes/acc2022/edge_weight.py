943e110f261e5c8eeb0972a56eaad586a562e9e2
fpresenza <francisco.presenza@gmail.com>
2023-07-18 12:02:08 -0300

    imagenes/acc2022/edge_weight: more improvements

--- ejemplos/imagenes/acc2022/edge_weight.py
+++ ejemplos/imagenes/acc2022/edge_weight.py
@@ -15,7 +15,7 @@ plt.rcParams['ps.fonttype'] = 42
 plt.rcParams['mathtext.fontset'] = 'dejavuserif'
 plt.rcParams['font.family'] = 'serif'
 
-fig, ax = plt.subplots(figsize=(3.5, 2.))
+fig, ax = plt.subplots(figsize=(4, 2.))
 fig.subplots_adjust(left=0.14, bottom=0.32)
 ax.grid(1)
 ax.minorticks_on()
@@ -27,24 +27,25 @@ ax.tick_params(
 
 d = np.linspace(0, 2, 200)
 print(d[::50])
-w = logistic(d, steepness=20, midpoint=1)
-ax.plot(d, w, label=r'$\beta = 20 / \rho$')
-w = logistic(d, steepness=40, midpoint=1)
-ax.plot(d, w, label=r'$\beta = 40 / \rho$')
+w = logistic(d, steepness=40, midpoint=0.9)
+ax.plot(d, w, label=r'$\beta = 40 / \rho, \tilde{\rho} = 0.9 \rho$')
+w = logistic(d, steepness=20, midpoint=1.1)
+ax.plot(d, w, label=r'$\beta = 20 / \rho, \tilde{\rho} = 1.1 \rho$')
 
 ax.vlines(1, 0, 1, color='k', alpha=0.6, ls='--', lw=0.5)
-ax.axvspan(0, 1, color='green', alpha=0.2)
-ax.axvspan(1, 2, color='red', alpha=0.2)
+ax.axvspan(0, 1, color='green', alpha=0.15)
+ax.axvspan(1, 2, color='red', alpha=0.15)
 ax.set_xlim(0, 2)
 ax.set_xlabel(r'$\Vert p_i - p_j \Vert \; / \; \rho$', fontsize=10)
-ax.set_ylabel(r'$w(i, j)}$', fontsize=10)
+ax.set_ylabel(r'$w_{ij}$', fontsize=10)
 ax.set_xticklabels([0, 0.5, 1, 1.5, 2])
 ax.set_xticks([0, 0.5, 1, 1.5, 2])
 ax.set_yticklabels([0, 0.5, 1])
 ax.set_yticks([0, 0.5, 1])
 ax.legend(
-    fontsize='small', handlelength=1.5,
-    labelspacing=0.5, borderpad=0.2)
+    fontsize='x-small', handlelength=1.5,
+    labelspacing=0.5, borderpad=0.2,
+    loc='upper right')
 # plt.show()
 
 fig.savefig('/tmp/edge_weight.png', format='png', dpi=360)
