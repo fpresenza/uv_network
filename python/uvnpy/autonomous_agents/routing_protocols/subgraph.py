9b7a68cde0936105d43d02aba633e9db3f511c07
fpresenza <francisco.presenza@gmail.com>
2024-02-07 10:59:50 -0300

    routing_protocols/subgraph: add comments

--- python/uvnpy/autonomous_agents/routing_protocols/subgraph.py
+++ python/uvnpy/autonomous_agents/routing_protocols/subgraph.py
@@ -36,7 +36,7 @@ class subgraph_protocol(object):
         El protocolo se basa en dos tipos de tokens: accion y estado.
 
         Cada nodo recibe un token de estado por cada nodo en su subgrafo,
-        computa las acciones de control, las empaqueta en un token de accion
+        computa las acciones de control, las empaqueta en un token de accions
         y las reenvia al mismo.
 
         Los tokens de accion se propagan hasta que la cantidad de hops
@@ -47,19 +47,20 @@ class subgraph_protocol(object):
         caminos por los cuales llegaron aquellos tokens de accion.
         """
         self.node_id = node_id
-        self.action = {}
-        self.state = {}
+        self.action = {}    # guarda los tokens de accion recibidos (fifo)
+        self.state = {}    # guarda los tokens de estado recibidos (fifo)
 
     def action_tokens(self):
+        # Extrae los tokens de accion de la queue
         return tuple(self.action.values())
 
     def state_tokens(self):
+        # Extrae los tokens de estado de la queue
         return tuple(self.state.values())
 
-    def to_target(self, token):
-        return token.hops_travelled < token.hops_to_target
-
     def extract_action(self):
+        """ Extrae los datos encontrados en los token de accion que tienen
+        info para este nodo en el campo data"""
         cmd = {
             token.center: token.data[self.node_id]
             for token in self.action.values()
@@ -67,6 +68,9 @@ class subgraph_protocol(object):
         return cmd
 
     def extract_state(self, key, hops):
+        """ Extrae los datos asociados a key encontrados en los token de
+        estado que hayan atravesado una cantidad de enlaces menor o igual
+        a hops"""
         p = {
             token.center: token.data[key]
             for token in self.state.values()
@@ -74,6 +78,8 @@ class subgraph_protocol(object):
         return p
 
     def geodesics(self, hops):
+        """Calcula las distancias geodesicas de otros nodos en funcion de los
+        hops atravesados por sus mensajes"""
         g = {
             token.center: token.hops_travelled
             for token in self.state.values()
@@ -82,12 +88,14 @@ class subgraph_protocol(object):
 
     def broadcast(self, timestamp, action, state, extent):
         """Prepara las listas con los tokens que se deben enviar.
-        Luego elimina todos los tokens recibidos de otros nodos."""
+        Luego elimina todos los tokens recibidos de otros nodos"""
+        # Tokens de accion de otros nodos para retransmitir
         action_tokens = {
             token.center: token
             for token in self.action.values()
-            if self.to_target(token)}
+            if token.hops_travelled < token.hops_to_target}
 
+        # Token de accion propio para transmitir
         action_tokens[self.node_id] = Token(
             center=self.node_id,
             timestamp=timestamp,
@@ -95,10 +103,12 @@ class subgraph_protocol(object):
             hops_travelled=0,
             data=action.copy())
 
+        # Tokens de estado de otros nodos para retransmitir
         state_tokens = {
             token.center: token
             for token in self.state.values()}
 
+        # Token de estado propio para transmitir
         state_tokens[self.node_id] = Token(
             center=self.node_id,
             timestamp=timestamp,
@@ -111,8 +121,9 @@ class subgraph_protocol(object):
         return action_tokens, state_tokens
 
     def update_action(self, tokens):
+        """Actualiza la informacion de accion que es recibida en
+        forma de tokens"""
         for token in tokens:
-            """Actualiza la informacion de accion que es recibida"""
             if token.center != self.node_id:
                 """Actualiza los hops atravesados"""
                 token = token._replace(hops_travelled=token.hops_travelled + 1)
@@ -127,8 +138,9 @@ class subgraph_protocol(object):
                     self.action[center] = token
 
     def update_state(self, tokens):
+        """Actualiza la informacion de estado que es recibida en
+        forma de tokens"""
         for token in tokens:
-            """Actualiza la informacion de estado que es recibida"""
             if token.center != self.node_id:
                 """Actualiza los hops atravesados"""
                 token = token._replace(hops_travelled=token.hops_travelled + 1)
