208d21a599a16b06b66b462d09fabc9bc63a8ed8
fpresenza <francisco.presenza@gmail.com>
2024-04-27 16:15:35 -0300

    rm unnecesary rigidly linked check in sparse search

--- python/uvnpy/rsn/rigidity.py
+++ python/uvnpy/rsn/rigidity.py
@@ -11,11 +11,8 @@ import itertools
 from transformations import unit_vector
 from numba import njit
 
-from uvnpy.network.core import adjacency_from_geodesics
-from uvnpy.network.subsets import (
-    geodesics as fast_geodesics,
-    multihop_subframework)
-from uvnpy.rsn.distances import matrix as distance_matrix
+from uvnpy.network import subsets
+from uvnpy.rsn import distances
 
 
 THRESHOLD = 1e-4
@@ -262,7 +259,7 @@ def extents(A, p, threshold=THRESHOLD):
         h = 0
         while not minimum_found:
             h += 1
-            Ai, xi = multihop_subframework(A, p, i, h)
+            Ai, xi = subsets.multihop_subframework(A, p, i, h)
             Si = symmetric_matrix(Ai, xi)
             re = np.linalg.eigvalsh(Si)[f]
             if re > threshold:
@@ -274,7 +271,7 @@ def extents(A, p, threshold=THRESHOLD):
 def fast_extents(A, p, threshold=THRESHOLD):
     n, d = p.shape
     f = d * (d + 1) // 2
-    geodesics = fast_geodesics(A)
+    geodesics = subsets.geodesics(A)
     hops = np.empty(n, dtype=int)
     for i in range(n):
         minimum_found = False
@@ -292,58 +289,13 @@ def fast_extents(A, p, threshold=THRESHOLD):
     return hops
 
 
-def superframework_extent(A, hops):
-    n = len(A)
-    geodesics = fast_geodesics(A)
-    return np.array(
-        [np.max(geodesics[i][geodesics[i] <= hops]) for i in range(n)],
-        dtype=int
-    )
-
-
-def bidirectional_extents(A, p, threshold=THRESHOLD):
-    """Compute both subframework and superframework extent
-
-    Returns
-    -------
-    sub_extent: int array
-        the extent of the subframeworks
-    super_extent: int array
-        the extent of the superframeworks
-    """
-    n, d = p.shape
-    f = d * (d + 1) // 2
-    geodesics = fast_geodesics(A)
-    sub_extent = np.empty(n, dtype=int)
-    for i in range(n):
-        minimum_found = False
-        h = 0
-        while not minimum_found:
-            h += 1
-            subset = geodesics[i] <= h
-            Ai = A[np.ix_(subset, subset)]
-            pi = p[subset]
-            Si = fast_symmetric_matrix(Ai, pi)
-            re = np.linalg.eigvalsh(Si)[f]
-            if re > threshold:
-                minimum_found = True
-        sub_extent[i] = h
-
-    super_extent = np.array(
-        [np.max(geodesics[i][geodesics[i] <= sub_extent]) for i in range(n)],
-        dtype=int
-    )
-
-    return sub_extent, super_extent
-
-
 def minimum_radius(A, p, threshold=THRESHOLD, return_radius=False):
     """Add or delete edges to a framework until it is radius-wise minimally
     rigid."""
     A = A.copy()
     n, d = p.shape
     f = d * (d + 1) // 2
-    dist = distance_matrix(p)
+    dist = distances.matrix(p)
 
     if is_inf_rigid(A, p, threshold):
         B = A.copy()
@@ -378,257 +330,94 @@ def minimum_radius(A, p, threshold=THRESHOLD, return_radius=False):
         return A
 
 
-def rigidly_linked(geodesics, p, extents, threshold=THRESHOLD):
-    """Determines whether a set of subframeworks cover the whole framework and
-    is rigidly linked
-
-    Returns:
-        Adjacency matrix of the link graph
-
-    Raise:
-        ValueError: whenever condition is not satisfied
-    """
-    n, d = p.shape
-    A = adjacency_from_geodesics(geodesics)
-    centers = np.nonzero(extents)[0]
-    subsets = geodesics[centers] <= extents[centers].reshape(-1, 1)
-
-    # check if union of subgraphs cover all vertices
-    is_in_count = np.sum(subsets, axis=0)
-    if np.any(is_in_count == 0):
-        raise ValueError('Node(s) not covered.')
-
-    # check if a single subgraph cover all vertices
-    if len(centers) == 1:
-        return 1 - np.eye(n)
-
-    # computes the union between subgraphs
-    Au = np.zeros((n, n))
-    for subset in subsets:
-        idx = np.ix_(subset, subset)
-        Au[idx] = A[idx].copy()
-
-    # link graph
-    Al = A - Au
-    # detect link nodes
-    shared_nodes = is_in_count > 1
-    link_edges = np.any(Al, axis=0)
-    link_nodes = np.logical_or(shared_nodes, link_edges)
-    for subset in subsets:
-        link_subset = np.logical_and(link_nodes, subset)
-        num_link = sum(link_subset)
-        if num_link < d:
-            raise ValueError('Subframework(s) hasn\'t got enough link nodes.')
-        Al[np.ix_(link_subset, link_subset)] = 1 - np.eye(num_link)
-
-    # check if the link graph is rigid
-    if not is_inf_rigid(
-            Al[np.ix_(link_nodes, link_nodes)], p[link_nodes], threshold):
-        raise ValueError('Link framework is flexible.')
-
-    return Al
-
-
-def rigidly_linked_by_vertices(geodesics, p, extents, threshold=THRESHOLD):
-    """Determines whether a set of subframeworks cover the whole framework and
-    is rigidly linked
-
-    Returns:
-        Adjacency matrix of the link graph
-
-    Raise:
-        ValueError: whenever condition is not satisfied
-    """
-    n, d = p.shape
-    centers = np.nonzero(extents)[0]
-    subsets = geodesics[centers] <= extents[centers].reshape(-1, 1)
-
-    # check if union of subgraphs cover all vertices
-    is_in_count = np.sum(subsets, axis=0)
-    if np.any(is_in_count == 0):
-        raise ValueError('Node(s) not covered.')
-
-    # check if a single subgraph cover all vertices
-    if len(centers) == 1:
-        return 1 - np.eye(n)
-
-    # link graph
-    Al = np.zeros((n, n))
-    # detect link nodes
-    link_nodes = is_in_count > 1
-    for subset in subsets:
-        link_subset = np.logical_and(link_nodes, subset)
-        num_link = sum(link_subset)
-        if num_link < d:
-            raise ValueError('Subframework(s) hasn\'t got enough link nodes.')
-        Al[np.ix_(link_subset, link_subset)] = 1 - np.eye(num_link)
-
-    # check if the link graph is rigid
-    if not is_inf_rigid(
-            Al[np.ix_(link_nodes, link_nodes)], p[link_nodes], threshold):
-        raise ValueError('Link framework is flexible.')
-
-    return Al
-
-
 def sparse_centers_full_search(
-        A, p, extents, metric, threshold=THRESHOLD, vertices_only=False):
-    """Dada un conjunto de extensiones, elimina subframeworks iterativamente
-    hasta que no puede eliminar mas dado que se pierde rigidez o no se cubre
-    todo el framework.
+        A, p, extents, max_extent, metric, threshold=THRESHOLD):
+    """Prueba todas las extensiones que no superen max(extents) y elige la de
+    menor metric.
 
     Requires:
+    ---------
         framework is rigid
     """
-    if vertices_only:
-        is_linked = rigidly_linked_by_vertices
-    else:
-        is_linked = rigidly_linked
-
     n = len(p)
-    geodesics = fast_geodesics(A)
-    degree = A.sum(axis=0)
-    max_hops = extents.max()
-    search_space = itertools.product(range(max_hops + 1), repeat=n)
+    geodesics = subsets.geodesics(A)
+    search_space = itertools.product(
+        *([0] + list(range(extents[i], max_extent + 1)) for i in range(n))
+    )
 
     min_value = np.inf
-    for h in search_space:
+    for h in list(search_space)[1:]:
         h = np.array(h)
-        if np.any(np.logical_and(h > 0, h < extents)):
-            continue
-        try:
-            is_linked(geodesics, p, h, threshold)
-            for i in np.nonzero(h)[0]:
-                Vi = geodesics[i] <= h[i]
-                Ai = A[Vi][:, Vi]
-                pi = p[Vi]
-                if not is_inf_rigid(Ai, pi, threshold):
-                    raise ValueError
-            new_value = metric(degree, h, geodesics)
-            if new_value < min_value:
-                min_value = new_value
-                h_opt = h
-        except ValueError:
-            pass
+        V = geodesics <= np.reshape(h, (-1, 1))
+        C = V[h > 0]
+        for i, c in enumerate(C):
+            k = np.delete(C, i, axis=0)
+            if np.all(~c + k, axis=1).any():
+                continue
+            Ai = A[c][:, c]
+            pi = p[c]
+            if not is_inf_rigid(Ai, pi, threshold):
+                # not ridigly subgraph
+                break
+        new_value = metric(geodesics, h)
+        if new_value < min_value:
+            min_value = new_value
+            h_opt = h
 
     return h_opt
 
 
-def sparse_centers_binary_search(
-        A, p, extents, metric, threshold=THRESHOLD, vertices_only=False):
-    """Dada un conjunto de extensiones, elimina subframeworks iterativamente
-    hasta que no puede eliminar mas dado que se pierde rigidez o no se cubre
-    todo el framework.
+def sparse_centers_binary_search(A, p, extents, metric, threshold=THRESHOLD):
+    """Dada un conjunto de extensiones, elimina uno por uno los subframeworks
+    alternadamente y elige la de menor metric.
 
     Requires:
+    ---------
         framework is rigid
     """
-    if vertices_only:
-        is_linked = rigidly_linked_by_vertices
-    else:
-        is_linked = rigidly_linked
-
     n = len(p)
-    geodesics = fast_geodesics(A)
-    degree = A.sum(axis=0)
+    geodesics = subsets.geodesics(A)
     search_factors = itertools.product((0, 1), repeat=n)
 
     min_value = np.inf
-    for f in search_factors:
+    for f in list(search_factors)[1:]:
         h = np.multiply(extents, f)
-        try:
-            is_linked(geodesics, p, h, threshold)
-            new_value = metric(degree, h, geodesics)
-            if new_value < min_value:
-                min_value = new_value
-                h_opt = h
-        except ValueError:
-            pass
+        new_value = metric(geodesics, h)
+        if new_value < min_value:
+            min_value = new_value
+            h_opt = h
 
     return h_opt
 
 
-def sparse_centers(
-        A, p, extents, metric, threshold=THRESHOLD, vertices_only=False):
-    """Dada un conjunto de extensiones, elimina subframeworks iterativamente
-    hasta que no puede eliminar mas dado que se pierde rigidez o no se cubre
-    todo el framework.
+def sparse_centers_greedy_search(A, p, extents, metric, threshold=THRESHOLD):
+    """Dada un conjunto de extensiones, elimina recursivamente el subframework
+    de mayor extension, hasta que no puede eliminar mas.
 
     Requires:
+    ---------
         framework is rigid
     """
-    if vertices_only:
-        is_linked = rigidly_linked_by_vertices
-    else:
-        is_linked = rigidly_linked
-
     n = len(p)
-    geodesics = fast_geodesics(A)
-    degree = A.sum(axis=0)
+    geodesics = subsets.geodesics(A)
     hops = extents.copy()
-    centers = np.arange(n)
-    min_found = False
-    while not min_found:
-        min_value = np.inf
+    remain = np.arange(n)
+    terminate = False
+
+    min_value = np.inf
+    while not terminate:
         remove = None
-        for i in centers:
+        for i in remain:
             sparsed = hops.copy()
             sparsed[i] = 0
-            try:
-                is_linked(geodesics, p, sparsed, threshold)
-                new_value = metric(degree, sparsed, geodesics)
-                if new_value < min_value:
-                    min_value = new_value
-                    remove = i
-            except ValueError:
-                pass
+            new_value = metric(geodesics, sparsed)
+            if new_value < min_value:
+                min_value = new_value
+                remove = i
 
-        if min_value < np.inf:
+        if remove is not None:
             hops[remove] = 0
-            centers = np.delete(centers, centers == remove)
+            remain = np.delete(remain, remain == remove)
         else:
-            min_found = True
-    return hops
-
-
-def sparse_centers_two_steps(
-        A, p, extents, metric, threshold=THRESHOLD, vertices_only=False):
-    """Dada un conjunto de extensiones, elimina subframeworks iterativamente
-    hasta que no puede eliminar mas dado que se pierde rigidez o no se cubre
-    todo el framework.
-
-    Requires:
-        framework is rigid
-    """
-    if vertices_only:
-        is_linked = rigidly_linked_by_vertices
-    else:
-        is_linked = rigidly_linked
-
-    geodesics = fast_geodesics(A)
-    degree = A.sum(axis=0)
-    hops = extents.copy()
-    for h in reversed(np.unique(hops)):
-        max_extent = np.where(hops == h)[0]
-        min_found = False
-        while not min_found:
-            min_value = np.inf
-            remove = None
-            for i in max_extent:
-                sparsed = hops.copy()
-                sparsed[i] = 0
-                try:
-                    is_linked(geodesics, p, sparsed, threshold)
-                    new_value = metric(degree, sparsed, geodesics)
-                    if new_value < min_value:
-                        min_value = new_value
-                        remove = i
-                except ValueError:
-                    pass
-
-            if min_value < np.inf:
-                hops[remove] = 0
-                max_extent = np.delete(max_extent, max_extent == remove)
-            else:
-                min_found = True
+            terminate = True
     return hops
