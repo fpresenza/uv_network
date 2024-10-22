bafb5ffbebc1050c5d1161e496b2bdfa9b06ab13
fpresenza <francisco.presenza@gmail.com>
2024-10-22 15:44:57 -0300

    xy_snapshots: efficient csv reading

--- tests/control/xy_snapshots.py
+++ tests/control/xy_snapshots.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import progressbar
 
+from uvnpy.toolkit import data
 from uvnpy.network import plot
 
 plt.rcParams['text.usetex'] = False
@@ -23,8 +24,16 @@ plt.rcParams['font.family'] = 'serif'
 # ------------------------------------------------------------------
 parser = argparse.ArgumentParser(description='')
 parser.add_argument(
-    '-s', '--skip',
-    default=1, type=int, help='communication step in milli seconds'
+    '-i', '--init',
+    default=1, type=float, help='init time in milli seconds'
+)
+parser.add_argument(
+    '-e', '--end',
+    default=1, type=float, help='end time in milli seconds'
+)
+parser.add_argument(
+    '-j', '--jump',
+    default=1, type=int, help='numbers of frames jumped'
 )
 arg = parser.parse_args()
 
@@ -32,32 +41,34 @@ arg = parser.parse_args()
 # Read simulated data
 # ------------------------------------------------------------------
 t = np.loadtxt('data/t.csv', delimiter=',')
-x = np.loadtxt('data/position.csv', delimiter=',')
-A = np.loadtxt('data/adjacency.csv', delimiter=',')
-targets = np.loadtxt('data/targets.csv', delimiter=',')
-
-n = int(len(x[0])/2)
-n_targets = int(len(targets[0]) / 3)
-nodes = np.arange(n)
 
 # slices
-t = t[::arg.skip]
-x = x[::arg.skip]
-A = A[::arg.skip]
-targets = targets[::arg.skip]
+k_i = int(np.argmin(np.abs(t - arg.init)))
+k_e = int(np.argmin(np.abs(t - arg.end)))
 
-# reshapes
-x = x.reshape(-1, n, 2)
-A = A.reshape(-1, n, n)
-targets = targets.reshape(-1, n_targets, 3)
+x = data.read_csv(
+    'data/position.csv', rows=(k_i, k_e), jump=arg.jump, dtype=float
+)
+A = data.read_csv(
+    'data/adjacency.csv', rows=(k_i, k_e), jump=arg.jump, dtype=float
+)
+targets = data.read_csv(
+    'data/targets.csv', rows=(k_i, k_e), jump=arg.jump, dtype=float
+)
+
+N = len(x)
+
+n = int(len(x[0])/2)
+nodes = np.arange(n)
 
 # ------------------------------------------------------------------
 # Plot snapshots
 # ------------------------------------------------------------------
 lim = 1000
-bar = progressbar.ProgressBar(maxval=np.round(t[-1], 3)).start()
+bar = progressbar.ProgressBar(maxval=N).start()
 
-for k, tk in enumerate(t):
+for k in range(N):
+    tk = t[k_i + k]
     fig, ax = plt.subplots(figsize=(5, 5))
     ax.tick_params(
         axis='both',       # changes apply to the x-axis
@@ -77,29 +88,33 @@ for k, tk in enumerate(t):
             verticalalignment='bottom', horizontalalignment='left',
             transform=ax.transAxes, color='r', fontsize=8)
 
+    x_k = x[k].reshape(n, 2)
     for i in nodes:
         plot.nodes(
-            ax, x[k, i],
+            ax, x_k[i],
             color='k',
             marker=f'${i}$',
             s=20,
             lw=0.2
         )
-        circle = plt.Circle(x[k, i], 30.0, alpha=0.3)
+        circle = plt.Circle(x_k[i], 30.0, alpha=0.3)
         ax.add_artist(circle)
-    plot.edges(ax, x[k], A[k], color=cm.coolwarm(20), lw=0.5, zorder=0)
+    plot.edges(
+        ax, x_k, A[k].reshape(n, n), color=cm.coolwarm(20), lw=0.5, zorder=0
+    )
 
-    untracked = targets[k, :, 2].astype(bool)
+    targets_k = targets[k].reshape(-1, 3)
+    untracked = targets_k[:, 2].astype(bool)
     tracked = np.logical_not(untracked)
     ax.scatter(
-        targets[k, untracked, 0], targets[k, untracked, 1],
+        targets_k[untracked, 0], targets_k[untracked, 1],
         marker='s', s=4, color='0.6')
     ax.scatter(
-        targets[k, tracked, 0], targets[k, tracked, 1],
+        targets_k[tracked, 0], targets_k[tracked, 1],
         marker='s', s=4, color='green')
 
     fig.savefig('data/snapshots/{}.png'.format(k), format='png', dpi=360)
     plt.close()
-    bar.update(np.round(tk, 3))
+    bar.update(k)
 
 bar.finish()
