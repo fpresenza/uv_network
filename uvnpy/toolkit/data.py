c5f0d86d9a0615b6e547d385b698eddad95eee3e
fpresenza <francisco.presenza@gmail.com>
2024-10-23 12:23:55 -0300

    read_data_from_csv:  add reshape and conversion to np.array

--- uvnpy/toolkit/data.py
+++ uvnpy/toolkit/data.py
@@ -2,7 +2,14 @@ import csv
 import numpy as np
 
 
-def read_csv(file_path, rows=(0, 1), jump=1, dtype=object):
+def read_csv(
+        file_path,
+        rows=(0, 1),
+        jump=1,
+        dtype=object,
+        shape=-1,
+        asarray=False
+        ):
     data = []
 
     with open(file_path, mode='r') as file:
@@ -12,9 +19,12 @@ def read_csv(file_path, rows=(0, 1), jump=1, dtype=object):
         for i, row in enumerate(reader):
             if (i % jump == 0):
                 if i >= rows[0] and i < rows[1]:
-                    data.append(np.array(row).astype(dtype))
+                    data.append(np.array(row).astype(dtype).reshape(shape))
                     k += 1
                 elif i >= rows[1]:
                     break
 
-    return data
+    if asarray:
+        return np.asarray(data)
+    else:
+        return data
