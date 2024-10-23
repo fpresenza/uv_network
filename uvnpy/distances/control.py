0087fac846d5012d29172ecb278a6ff4dbe3b2aa
fpresenza <francisco.presenza@gmail.com>
2024-10-23 17:41:47 -0300

    RigidityMaintenance: all in one class

--- uvnpy/distances/control.py
+++ uvnpy/distances/control.py
@@ -11,30 +11,59 @@ from uvnpy.distances import core
 from uvnpy.toolkit import functions
 
 
-class RigidityMaintenancePowEig(object):
+class RigidityMaintenance(object):
     """
-    Rigidity Maintenance Control based on the minimization of the
-    inverse of the rigidity eigenvalue.
+    Rigidity Maintenance Control based on the minimization
+    of the rigidity matrix (nonzero) eigenvalues' inverses.
 
     args:
     -----
         dim: realization space dimension
         dmax: maximum connectivity distance
         steepness: connectivity decrease factor
-        power: positive number to exponentiate the ridigity eigenvalue
+        power: positive number to exponentiate the eigenvalues
         adjacent_only: bool to consider non-adjacent vertices relations
+        eigenvalues: which ones to consider (min of all)
+        functional: wich function of the einengvalues (power or logarithmic)
     """
-    def __init__(self, dim, dmax, steepness, power, adjacent_only=False):
+    def __init__(
+            self,
+            dim,
+            dmax,
+            steepness,
+            power=1.0,
+            adjacent_only=False,
+            eigenvalues='min',
+            functional='pow'
+            ):
         self.dim = dim
         self.midpoint = dmax
         self.steepness = steepness
-        self.r = power
+        self.r = abs(power)
         self.dof = int(dim * (dim + 1)/2)
         self.adjacent_only = adjacent_only
 
-    def gradient(self, matrix_deriv, eigenvalue, eigenvector):
-        dlambda_dx = eigenvector.dot(matrix_deriv).dot(eigenvector)
-        return -self.r * eigenvalue**(-self.r - 1) * dlambda_dx
+        if eigenvalues == 'min':
+            self.eig_max = lambda x: self.dof + 1
+        elif eigenvalues == 'all':
+            self.eig_max = lambda x: x.size
+        else:
+            ValueError('Invalid selection of eigenvalues.')
+
+        if functional == 'pow':
+            self.gradient = self.gradient_pow
+        elif functional == 'log':
+            self.gradient = self.gradient_log
+        else:
+            ValueError('Invalid selection of functional.')
+
+    def gradient_pow(self, matrix_deriv, eigenvalue, eigenvector):
+        eigenvalue_deriv = eigenvector.dot(matrix_deriv).dot(eigenvector)
+        return - self.r * eigenvalue_deriv / eigenvalue**(self.r + 1)
+
+    def gradient_log(self, matrix_deriv, eigenvalue, eigenvector):
+        eigenvalue_deriv = eigenvector.dot(matrix_deriv).dot(eigenvector)
+        return - eigenvalue_deriv / eigenvalue
 
     def weighted_rigidity_matrix(self, x):
         w = core.distance_matrix(x)
@@ -48,50 +77,11 @@ class RigidityMaintenancePowEig(object):
         S = self.weighted_rigidity_matrix(x)
         e, V = np.linalg.eigh(S)
         dS_dx = functions.derivative_eval(self.weighted_rigidity_matrix, x)
-        grad = self.gradient(dS_dx, e[self.dof], V[:, self.dof])
-        return -grad.reshape(x.shape)
-
-
-class RigidityMaintenanceLogDet(object):
-    """
-    Rigidity Maintenance Control based on the minimization of the
-    logarithm of the product of all nonzero laplacian eigenvalues.
-
-    args:
-    -----
-        dim: realization space dimension
-        dmax: maximum connectivity distance
-        steepness: connectivity decrease factor
-        adjacent_only: bool to consider non-adjacent vertices relations
-    """
-    def __init__(self, dim, dmax, steepness, adjacent_only=False):
-        self.dim = dim
-        self.midpoint = dmax
-        self.steepness = steepness
-        self.dof = int(dim * (dim + 1)/2)
-        self.adjacent_only = adjacent_only
-
-    def gradient(self, matrix_deriv, eigenvalue, eigenvector):
-        dlambda_dx = eigenvector.dot(matrix_deriv).dot(eigenvector)
-        return - dlambda_dx / eigenvalue
-
-    def weighted_rigidity_matrix(self, x):
-        w = core.distance_matrix(x)
-        if self.adjacent_only:
-            w[w > self.midpoint] = 0.0
-        w[w > 0] = functions.logistic(w[w > 0], self.midpoint, self.steepness)
-        S = core.rigidity_laplacian_multiple_axes(w, x)
-        return S
-
-    def update(self, x):
-        S = self.weighted_rigidity_matrix(x)
-        e, V = np.linalg.eigh(S)
-        dS_dx = functions.derivative_eval(self.weighted_rigidity_matrix, x)
-        grad = [
+        grad = sum([
             self.gradient(dS_dx, e[k], V[:, k])
-            for k in range(self.dof, x.size)
-        ]
-        return - sum(grad).reshape(x.shape)
+            for k in range(self.dof, self.eig_max(x))
+        ])
+        return - grad.reshape(x.shape)
 
 
 class CommunicationLoad(object):
