f54cdd29af5fc57dd4f2a2c80702afa12e2d131f
fpresenza <francisco.presenza@gmail.com>
2025-03-12 12:45:15 -0300

    wip: decomposition plots

--- tests/bearings/figs/dense_decomposition_metrics_plot.py
+++ tests/bearings/figs/dense_decomposition_metrics_plot.py
@@ -3,11 +3,27 @@
 """ Created on
 @author: fran
 """
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
 from uvnpy.toolkit.data import read_csv
 
+
+# ------------------------------------------------------------------
+# Parseo de argumentos
+# ------------------------------------------------------------------
+parser = argparse.ArgumentParser(description='')
+parser.add_argument(
+    '-r', '--rep', type=int, help='number of repetitions'
+)
+
+arg = parser.parse_args()
+
+# ------------------------------------------------------------------
+# Configuración
+# ------------------------------------------------------------------
+rep = arg.rep
 
 np.set_printoptions(suppress=True, precision=4, linewidth=250)
 plt.rcParams['text.usetex'] = False
@@ -33,8 +49,8 @@ hb_count = read_csv('/tmp/hb_count.csv', rows=(0, np.inf), dtype=int)
 # ------------------------------------------------------------------
 # Distances
 # ------------------------------------------------------------------
-fig, ax = plt.subplots(figsize=(2.5, 2))
-fig.subplots_adjust(bottom=0.2, left=0.2)
+fig, ax = plt.subplots(figsize=(2.5, 1.5))
+fig.subplots_adjust(bottom=0.25, left=0.2)
 ax.tick_params(
     axis='both',       # changes apply to the x-axis
     which='both',      # both major and minor ticks are affected
@@ -43,8 +59,8 @@ ax.tick_params(
 )
 ax.grid(lw=0.4)
 ax.set_ylim(0.0, 100.0)
-ax.set_xlabel(r'#(nodes)', fontsize=9)
-ax.set_ylabel(r'%(subframeworks)', fontsize=9)
+ax.set_xlabel(r'$n$', fontsize=9)
+ax.set_ylabel(r'(%)', fontsize=9, labelpad=3)
 # ax.plot(
 #     nodes, [np.median(np.repeat(u, c)) for u, c in zip(hd, hd_count)],
 #     label=r'$\eta_{\mathrm{dist}}$', lw=0.8
@@ -54,40 +70,40 @@ two_hop = []
 three_hop = []
 for n, u, c in zip(nodes, hd, hd_count):
     if 1 in u:
-        one_hop.append(c[u == 1] * 100 / (50 * n))
+        one_hop.append(c[u == 1] * 100 / (rep * n))
     else:
         one_hop.append(0)
     if 2 in u:
-        two_hop.append(c[u == 2] * 100 / (50 * n))
+        two_hop.append(c[u == 2] * 100 / (rep * n))
     else:
         two_hop.append(0)
     if 3 in u:
-        three_hop.append(c[u == 3] * 100 / (50 * n))
+        three_hop.append(c[u == 3] * 100 / (rep * n))
     else:
         three_hop.append(0)
 ax.plot(
     nodes, one_hop,
-    label=r'$1$', lw=0.7, marker='o', markersize=3,
+    label=r'$n_1$', lw=0.7, marker='o', markersize=3, markevery=5
 )
 ax.plot(
     nodes, two_hop,
-    label=r'$2$', lw=0.7, marker='s', markersize=3,
+    label=r'$n_2$', lw=0.7, marker='s', markersize=3, markevery=5
 )
 ax.plot(
     nodes, three_hop,
-    label=r'$3$', lw=0.7, marker='x', markersize=3,
+    label=r'$n_3$', lw=0.7, marker='x', markersize=3, markevery=5
 )
 ax.legend(
     fontsize='x-small', handlelength=1,
-    labelspacing=0.5, borderpad=0.2
+    labelspacing=0.3, borderpad=0.2, loc='center right'
 )
-fig.savefig('/tmp/distance_extents.png', format='png', dpi=400)
+fig.savefig('/tmp/distance_extents.png', format='png', dpi=600)
 
 # ------------------------------------------------------------------
 # Bearings
 # ------------------------------------------------------------------
-fig, ax = plt.subplots(figsize=(2.5, 2))
-fig.subplots_adjust(bottom=0.2, left=0.2)
+fig, ax = plt.subplots(figsize=(2.5, 1.5))
+fig.subplots_adjust(bottom=0.25, left=0.2)
 ax.tick_params(
     axis='both',       # changes apply to the x-axis
     which='both',      # both major and minor ticks are affected
@@ -95,8 +111,8 @@ ax.tick_params(
     labelsize='x-small')
 ax.grid(lw=0.4)
 ax.set_ylim(0.0, 100.0)
-ax.set_xlabel(r'#(nodes)', fontsize=9)
-ax.set_ylabel(r'%(subframeworks)', fontsize=9)
+ax.set_xlabel(r'$n$', fontsize=9)
+ax.set_ylabel(r'(%)', fontsize=9, labelpad=3)
 # ax.plot(
 #     nodes, [np.median(np.repeat(u, c)) for u, c in zip(hb, hb_count)],
 #     label=r'$\eta_{\mathrm{bear}}$', lw=0.8
@@ -106,31 +122,43 @@ two_hop = []
 three_hop = []
 for n, u, c in zip(nodes, hb, hb_count):
     if 1 in u:
-        one_hop.append(c[u == 1] * 100 / (50 * n))
+        one_hop.append(c[u == 1] * 100 / (rep * n))
     else:
         one_hop.append(0)
     if 2 in u:
-        two_hop.append(c[u == 2] * 100 / (50 * n))
+        two_hop.append(c[u == 2] * 100 / (rep * n))
     else:
         two_hop.append(0)
     if 3 in u:
-        three_hop.append(c[u == 3] * 100 / (50 * n))
+        three_hop.append(c[u == 3] * 100 / (rep * n))
     else:
         three_hop.append(0)
 ax.plot(
     nodes, one_hop,
-    label=r'$1$', lw=0.7, marker='o', markersize=3,
+    label=r'$n_1$', lw=0.7, marker='o', markersize=3, markevery=5
 )
 ax.plot(
     nodes, two_hop,
-    label=r'$2$', lw=0.7, marker='s', markersize=3,
+    label=r'$n_2$', lw=0.7, marker='s', markersize=3, markevery=5
 )
 ax.plot(
     nodes, three_hop,
-    label=r'$3$', lw=0.7, marker='x', markersize=3,
+    label=r'$n_3$', lw=0.7, marker='x', markersize=3, markevery=5
 )
 ax.legend(
     fontsize='x-small', handlelength=1,
-    labelspacing=0.5, borderpad=0.2
+    labelspacing=0.3, borderpad=0.2, loc='center'
 )
-fig.savefig('/tmp/bearing_extents.png', format='png', dpi=400)
+
+# ax2 = ax.twinx()
+# ax2.tick_params(
+#     axis='both',       # changes apply to the x-axis
+#     which='both',      # both major and minor ticks are affected
+#     pad=1,
+#     labelsize='x-small')
+# ax2.set_yticks([1, 2, 3, 4])
+# ax2.plot(
+#     nodes, [np.mean(np.repeat(u, c)) for u, c in zip(diam, diam_count)],
+#     label=r'$\Delta(\mathcal{G})$', lw=0.8, color='k'
+# )
+fig.savefig('/tmp/bearing_extents.png', format='png', dpi=600)
