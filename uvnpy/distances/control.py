3760eeb8e8a950e665507b971072125ed42f7792
fpresenza <francisco.presenza@gmail.com>
2025-03-14 16:48:32 -0300

    add collision avoidance with vanishing action

--- uvnpy/distances/control.py
+++ uvnpy/distances/control.py
@@ -128,3 +128,31 @@ class CollisionAvoidance(object):
         e = self.power
         neg_grad = e * (d - self.dmin)**(-e - 1) * r / d
         return neg_grad.sum(axis=0)
+
+
+class CollisionAvoidanceVanishing(object):
+    """
+    Gradient based Collision Avoidance.
+
+    args:
+    -----
+        power: positive number to exponentiate the distance
+        dmin: minimum allowed distance
+        dmax: distance at with the controla action vanish
+
+    """
+    def __init__(self, power=2.0, dmin=0.0, dmax=1.0):
+        self.power = power
+        self.dmin = dmin
+        self.dmax = dmax
+
+    def update(self, x, obstacles):
+        r = x - obstacles
+        d = np.sqrt(np.square(r).sum(axis=-1))
+        d = d.reshape(-1, 1)
+        e = self.power
+        dm = d - self.dmin
+        dM = d - self.dmax
+        deriv = dM / dm**(e + 1) * (2*dm - e * dM)
+        neg_grad = - deriv * (r / d)
+        return neg_grad.sum(axis=0)
