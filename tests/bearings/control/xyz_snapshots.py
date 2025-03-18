d5e01a118e709a4eb8b6942d822ea5c33436646b
fpresenza <francisco.presenza@gmail.com>
2025-03-17 23:02:10 -0300

    wip: add snapshot composition

--- tests/bearings/control/xyz_snapshots.py
+++ tests/bearings/control/xyz_snapshots.py
@@ -113,7 +113,7 @@ for k in range(N):
     # ax.set_xticklabels([])
     # ax.set_yticklabels([])
     # ax.set_zticklabels([])
-    ax.view_init(elev=20.0, azim=-50.0)
+    ax.view_init(elev=15.0, azim=-70.0)
 
     plot.nodes(
         ax, x[k],
@@ -182,7 +182,7 @@ for k in range(N):
     fig.savefig(
         'data/snapshots/frame{}.png'.format(str(k).zfill(3)),
         format='png',
-        dpi=360,
+        dpi=400,
         bbox_inches="tight",
         transparent=True
     )
@@ -190,3 +190,105 @@ for k in range(N):
     bar.update(k)
 
 bar.finish()
+
+fig = plt.figure(figsize=(8, 4))
+ax = fig.add_subplot(projection="3d")
+fig.subplots_adjust(right=0.5)
+# fig.set_size_inches(10, 8)  # Width = 10, Height = 8
+fig.tight_layout()
+ax.tick_params(
+    axis='both',       # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    pad=-2,
+    labelsize='xx-small'
+)
+ax.set_aspect('equal')
+ax.set_xlabel(r'$x \ (\mathrm{m})$', fontsize='xx-small', labelpad=-2.0)
+ax.set_ylabel(r'$y \ (\mathrm{m})$', fontsize='xx-small', labelpad=-2.0)
+ax.set_zlabel(r'$z \ (\mathrm{m})$', fontsize='xx-small', labelpad=-9.0)
+# ax.zaxis.labelpad = 0
+
+ax.set_xlim3d(0, 100.0)
+ax.set_ylim3d(0, 100.0)
+ax.set_zlim3d(0, 50.0)
+ax.set_xticks([0.0, 50.0, 100.0])
+ax.set_yticks([0.0, 50.0, 100.0])
+ax.set_zticks([0.0, 25.0, 50.0])
+# ax.set_xticklabels([])
+# ax.set_yticklabels([])
+# ax.set_zticklabels([])
+ax.view_init(elev=10.0, azim=-70.0)
+
+for i, k in enumerate([0, 50, 150]):
+    plot.nodes(
+        ax, x[k],
+        color=['r', 'g', 'b'][i],
+        marker='x',
+        s=20,
+        lw=1,
+        zorder=10,
+        alpha=1
+    )
+    plot.edges(
+        ax,
+        x[k],
+        A[k],
+        color='0.0',
+        alpha=0.5,
+        lw=0.5,
+        zorder=0
+    )
+
+untracked = targets[k][:, 3].astype(bool)
+tracked = np.logical_not(untracked)
+ax.scatter(
+    targets[k][untracked, 0],
+    targets[k][untracked, 1],
+    targets[k][untracked, 2],
+    marker='d',
+    linewidth=0,
+    edgecolor='black',
+    facecolor='black',
+    alpha=1,
+    s=6,
+)
+ax.scatter(
+    targets[k][tracked, 0],
+    targets[k][tracked, 1],
+    targets[k][tracked, 2],
+    marker='d',
+    linewidth=0.2,
+    edgecolor='black',
+    facecolor='none',
+    s=3,
+)
+ax.xaxis._axinfo['grid'].update(
+    color='0.5',
+    linestyle='-',
+    linewidth=0.25,
+    alpha=0.5
+)
+ax.yaxis._axinfo['grid'].update(
+    color='0.5',
+    linestyle='-',
+    linewidth=0.25,
+    alpha=0.5
+)
+ax.zaxis._axinfo['grid'].update(
+    color='0.5',
+    linestyle='-',
+    linewidth=0.25,
+    alpha=0.5
+)
+
+# Shorten the Z-axis (flatten effect)
+ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([1, 1, 0.5, 1]))
+ax.set_box_aspect(None, zoom=0.85)
+
+fig.savefig(
+    'data/snapshots/snapshots.png',
+    format='png',
+    dpi=400,
+    bbox_inches="tight",
+    transparent=True
+)
