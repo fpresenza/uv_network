9115aa15118242830dc0aa4b4562dbbcbdea3622
fpresenza <francisco.presenza@gmail.com>
2025-04-24 21:43:13 -0300

    wip: rm adjacency histeresis

--- uvnpy/network/disk_graph.py
+++ uvnpy/network/disk_graph.py
@@ -31,22 +31,3 @@ def adjacency_from_positions(p, dmax=np.inf):
     A[A > dmax**2] = 0
     A[A != 0] = 1
     return A
-
-
-def adjacency_histeresis(connected, p, dmin, dmax):
-    """
-    Disk proximity matrix with histeresis.
-
-    args:
-    -----
-        connected : (n, n) boolean array
-        p         : (n, d) state array
-        dmax      : maximum connectivity distance
-    """
-    r = p[:, None] - p
-    d2 = np.square(r).sum(axis=-1)
-    close = np.logical_and(1e-5 < d2, d2 < dmin**2)
-    far = d2 > dmax**2
-    between = np.logical_not(np.logical_or(close, far))
-    now_connected = np.logical_or(close, np.logical_and(connected, between))
-    return now_connected
