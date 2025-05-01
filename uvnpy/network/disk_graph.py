52a2ffdadf812d6572d42d3221f1eca12d95cba2
fpresenza <francisco.presenza@gmail.com>
2025-05-01 11:24:40 -0300

    new class: disk graph

--- uvnpy/network/disk_graph.py
+++ uvnpy/network/disk_graph.py
@@ -8,6 +8,51 @@
 import numpy as np
 
 
+class DiskGraph(object):
+    def __init__(self, dmax):
+        """
+        Class representing a disk-like state dependant graph.
+
+        args:
+        -----
+            dmax : max conextion range
+        """
+        self.dmax = np.reshape(dmax, (-1, 1))
+        self._adj = None
+
+    def adjacency_matrix(self):
+        return self._adj.copy()
+
+    def edge_set(self):
+        return np.argwhere(self._adj)
+
+    def is_edge(self, vertex_i, vertex_j):
+        return self._adj[vertex_i, vertex_j]
+
+    def share_edge(self, vertex_i, vertex_j):
+        return self._adj[vertex_i, vertex_j] or self._adj[vertex_j, vertex_i]
+
+    def out_neighbors(self, vertex):
+        return np.where(self._adj[vertex])[0]
+
+    def in_neighbors(self, vertex):
+        return np.where(self._adj[:, vertex])[0]
+
+    def update_adjacency_matrix(self, positions):
+        """
+        Disk graph adjacency matrix.
+
+        args:
+        -----
+            positions : (n, d) vector array
+        """
+        r = positions - positions[:, np.newaxis]
+        d = np.sqrt(np.square(r).sum(axis=-1))
+        self._adj = d <= self.dmax
+        self._adj[np.eye(len(positions), dtype=bool)] = False
+        return self._adj.copy()
+
+
 def edges_from_positions(p, dmax=np.inf):
     dmax = np.reshape(dmax, (-1, 1))
     r = p[:, np.newaxis] - p
