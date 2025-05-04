addb96d7081cc2bc9b4f31662ad51cb76f78acbf
fpresenza <francisco.presenza@gmail.com>
2025-05-04 18:21:46 -0300

    wip: xyz snapshots

--- tests/bearings/control/xyz_snapshots.py
+++ tests/bearings/control/xyz_snapshots.py
@@ -71,17 +71,10 @@ targets = data.read_csv(
     dtype=float,
     shape=(-1, 4)
 )
-action_extents = data.read_csv(
-    'data/action_extents.csv',
-    rows=(k_i, k_e),
-    jump=arg.jump,
-    dtype=float
-)
 
 N = len(x)
 
 n = len(x[0])
-nodes = np.arange(n)
 
 # ------------------------------------------------------------------
 # Plot snapshots
@@ -92,9 +85,9 @@ for k in range(N):
     tk = t[k]
     fig = plt.figure(figsize=(5, 5))
     ax = fig.add_subplot(projection="3d")
-    fig.subplots_adjust(right=0.5)
+    # fig.subplots_adjust(right=0.5)
     # fig.set_size_inches(10, 8)  # Width = 10, Height = 8
-    fig.tight_layout()
+    # fig.tight_layout()
     ax.tick_params(
         axis='both',       # changes apply to the x-axis
         which='both',      # both major and minor ticks are affected
@@ -109,27 +102,27 @@ for k in range(N):
 
     ax.set_xlim3d(0, 100.0)
     ax.set_ylim3d(0, 100.0)
-    ax.set_zlim3d(-50.0, 5.0)
+    ax.set_zlim3d(0.0, 50.0)
     ax.set_xticks([0.0, 25.0, 50.0, 75.0, 100.0])
     ax.set_yticks([0.0, 25.0, 50.0, 75.0, 100.0])
     ax.set_zticks([0.0, 25.0, 50.0])
     # ax.set_xticklabels([])
     # ax.set_yticklabels([])
     # ax.set_zticklabels([])
-    ax.view_init(elev=30.0, azim=-70.0)
+    ax.view_init(elev=20.0, azim=-70.0)
 
     ax.text(
-        1.0, 1.0, 1.0, r't = {:.3f}s'.format(tk),
+        120.0, 100.0, 150.0, r't = {:.3f}s'.format(tk),
         verticalalignment='bottom', horizontalalignment='left',
         transform=ax.transAxes, color='g', fontsize=8
     )
 
     p = x[k][:, :3]
-    th = x[k][:, 3]
+    psi = x[k][:, 3]
     for i in range(n):
-        axis = np.array([np.cos(th[i]), np.sin(th[i]), 0.0])
-        cone = geometry.cone(p[i], axis, 5.0, np.pi/3)
-        ax.add_collection3d(art3d.Poly3DCollection(cone, alpha=0.3))
+        axis = np.array([np.cos(psi[i]), np.sin(psi[i]), 0.0])
+        cone = geometry.cone(p[i], axis, 3.0, np.pi/3)
+        ax.add_collection3d(art3d.Poly3DCollection(cone, alpha=0.5))
 
     plot.nodes(
         ax, p,
@@ -193,8 +186,8 @@ for k in range(N):
     )
 
     # Shorten the Z-axis (flatten effect)
-    ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([1, 1, 0.5, 1]))
-    ax.set_box_aspect(None, zoom=0.85)
+    ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([1, 1, 0.55, 1]))
+    ax.set_box_aspect(None, zoom=1.0)
 
     fig.savefig(
         'data/snapshots/frame{}.png'.format(str(k + k_i_jump).zfill(3)),
@@ -207,105 +200,3 @@ for k in range(N):
     bar.update(k)
 
 bar.finish()
-
-# fig = plt.figure(figsize=(8, 4))
-# ax = fig.add_subplot(projection="3d")
-# fig.subplots_adjust(right=0.5)
-# # fig.set_size_inches(10, 8)  # Width = 10, Height = 8
-# fig.tight_layout()
-# ax.tick_params(
-#     axis='both',       # changes apply to the x-axis
-#     which='both',      # both major and minor ticks are affected
-#     pad=-2,
-#     labelsize='xx-small'
-# )
-# ax.set_aspect('equal')
-# ax.set_xlabel(r'$x \ (\mathrm{m})$', fontsize='xx-small', labelpad=-2.0)
-# ax.set_ylabel(r'$y \ (\mathrm{m})$', fontsize='xx-small', labelpad=-2.0)
-# ax.set_zlabel(r'$z \ (\mathrm{m})$', fontsize='xx-small', labelpad=-9.0)
-# # ax.zaxis.labelpad = 0
-
-# ax.set_xlim3d(0, 100.0)
-# ax.set_ylim3d(0, 100.0)
-# ax.set_zlim3d(0, 50.0)
-# ax.set_xticks([0.0, 50.0, 100.0])
-# ax.set_yticks([0.0, 50.0, 100.0])
-# ax.set_zticks([0.0, 25.0, 50.0])
-# # ax.set_xticklabels([])
-# # ax.set_yticklabels([])
-# # ax.set_zticklabels([])
-# ax.view_init(elev=10.0, azim=-70.0)
-
-# for i, k in enumerate([0, 50, 150]):
-#     plot.nodes(
-#         ax, x[k],
-#         color=['r', 'g', 'b'][i],
-#         marker='x',
-#         s=20,
-#         lw=1,
-#         zorder=10,
-#         alpha=1
-#     )
-#     plot.edges(
-#         ax,
-#         x[k],
-#         A[k],
-#         color='0.0',
-#         alpha=0.5,
-#         lw=0.5,
-#         zorder=0
-#     )
-
-# untracked = targets[k][:, 3].astype(bool)
-# tracked = np.logical_not(untracked)
-# ax.scatter(
-#     targets[k][untracked, 0],
-#     targets[k][untracked, 1],
-#     targets[k][untracked, 2],
-#     marker='d',
-#     linewidth=0,
-#     edgecolor='black',
-#     facecolor='black',
-#     alpha=1,
-#     s=6,
-# )
-# ax.scatter(
-#     targets[k][tracked, 0],
-#     targets[k][tracked, 1],
-#     targets[k][tracked, 2],
-#     marker='d',
-#     linewidth=0.2,
-#     edgecolor='black',
-#     facecolor='none',
-#     s=3,
-# )
-# ax.xaxis._axinfo['grid'].update(
-#     color='0.5',
-#     linestyle='-',
-#     linewidth=0.25,
-#     alpha=0.5
-# )
-# ax.yaxis._axinfo['grid'].update(
-#     color='0.5',
-#     linestyle='-',
-#     linewidth=0.25,
-#     alpha=0.5
-# )
-# ax.zaxis._axinfo['grid'].update(
-#     color='0.5',
-#     linestyle='-',
-#     linewidth=0.25,
-#     alpha=0.5
-# )
-
-# # Shorten the Z-axis (flatten effect)
-# ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([1, 1, 0.5, 1]))
-# ax.set_box_aspect(None, zoom=0.85)
-
-# fig.savefig(
-#     'data/snapshots/snapshots.png',
-#     format='png',
-#     dpi=400,
-#     bbox_inches="tight",
-#     transparent=True
-# )
---
+++ tests/bearings/control/xyz_snapshots_combined.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+import progressbar
+from mpl_toolkits.mplot3d import Axes3D
+import mpl_toolkits.mplot3d.art3d as art3d
+
+from uvnpy.toolkit import data, geometry
+from uvnpy.network import core, plot
+
+plt.rcParams['text.usetex'] = False
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
+plt.rcParams['mathtext.fontset'] = 'dejavuserif'
+plt.rcParams['font.family'] = 'serif'
+
+
+# ------------------------------------------------------------------
+# Parse arguments
+# ------------------------------------------------------------------
+parser = argparse.ArgumentParser(description='')
+parser.add_argument(
+    '-s', '--snaps',
+    nargs='+', type=int, help='snapshots'
+)
+arg = parser.parse_args()
+
+# ------------------------------------------------------------------
+# Read simulated data
+# ------------------------------------------------------------------
+t = np.loadtxt('data/t.csv', delimiter=',')
+T = len(t)
+# slices
+N = len(arg.snaps)
+ks = [int(np.argmin(np.abs(t - s))) for s in arg.snaps]
+
+t = t[ks]
+
+x = data.read_rows_csv(
+    'data/pose.csv',
+    rows=ks,
+    dtype=float,
+    shape=(-1, 4),
+    asarray=True
+)
+n = len(x[0])
+As = data.read_rows_csv(
+    'data/sens_adj.csv',
+    rows=ks,
+    dtype=float,
+    shape=(n, n),
+    asarray=True
+)
+Ac = data.read_rows_csv(
+    'data/comm_adj.csv',
+    rows=ks,
+    dtype=float,
+    shape=(n, n),
+    asarray=True
+)
+targets = data.read_rows_csv(
+    'data/targets.csv',
+    rows=ks,
+    dtype=float,
+    shape=(-1, 4),
+    asarray=True
+)
+# ------------------------------------------------------------------
+# Plot snapshots
+# ------------------------------------------------------------------
+bar = progressbar.ProgressBar(maxval=N).start()
+
+fig = plt.figure(figsize=(8, 4))
+ax = fig.add_subplot(projection="3d")
+fig.subplots_adjust(right=0.5)
+# fig.set_size_inches(10, 8)  # Width = 10, Height = 8
+fig.tight_layout()
+ax.tick_params(
+    axis='both',       # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    pad=-1,
+    labelsize='xx-small'
+)
+ax.set_aspect('equal')
+ax.set_xlabel(r'$x \ (\mathrm{m})$', fontsize='xx-small', labelpad=-12.0)
+ax.set_ylabel(r'$y \ (\mathrm{m})$', fontsize='xx-small', labelpad=-15.0)
+ax.set_zlabel(r'$z \ (\mathrm{m})$', fontsize='xx-small', labelpad=-15.0)
+# ax.zaxis.labelpad = 0
+
+ax.set_xlim3d(0, 100.0)
+ax.set_ylim3d(0, 100.0)
+ax.set_zlim3d(0, 50.0)
+ax.set_xticks([0.0, 50.0, 100.0])
+ax.set_yticks([0.0, 50.0, 100.0])
+ax.set_zticks([0.0, 25.0, 50.0])
+ax.set_xticklabels(['0.0', '', '100.0'])
+ax.set_yticklabels(['0.0', '', '100.0'])
+ax.set_zticklabels(['0.0', '', '50.0'])
+ax.view_init(elev=25.0, azim=-80.0)
+
+for k, s in enumerate(arg.snaps):
+    p = x[k][:, :3]
+    psi = x[k][:, 3]
+
+    plot.edges(
+        ax,
+        p,
+        core.as_undirected(As[k]).astype(int),
+        color='0.5',
+        alpha=1.0,
+        lw=0.4,
+        zorder=1
+    )
+    plot.edges(
+        ax,
+        p,
+        Ac[k] - core.as_undirected(As[k]).astype(int),
+        color='0.5',
+        alpha=1.0,
+        ls='--',
+        lw=0.6,
+        zorder=1
+    )
+    plot.nodes(
+        ax, p,
+        facecolor='C{}'.format(k+1),
+        edgecolor='none',
+        marker='o',
+        s=7,
+        # lw=1,
+        zorder=2,
+        # alpha=1
+    )
+    for i in range(n):
+        axis = np.array([np.cos(psi[i]), np.sin(psi[i]), 0.0])
+        cone = geometry.cone(p[i], axis, 3.0, np.pi/3)
+        ax.add_collection3d(art3d.Poly3DCollection(cone, alpha=0.6))
+    bar.update(k)
+
+untracked = targets[k][:, 3].astype(bool)
+tracked = np.logical_not(untracked)
+ax.scatter(
+    targets[k][untracked, 0],
+    targets[k][untracked, 1],
+    targets[k][untracked, 2],
+    marker='d',
+    linewidth=0,
+    edgecolor='black',
+    facecolor='black',
+    alpha=1,
+    s=6,
+)
+ax.scatter(
+    targets[k][tracked, 0],
+    targets[k][tracked, 1],
+    targets[k][tracked, 2],
+    marker='d',
+    linewidth=0.2,
+    edgecolor='black',
+    facecolor='none',
+    s=3,
+)
+ax.xaxis._axinfo['grid'].update(
+    color='0.5',
+    linestyle='-',
+    linewidth=0.25,
+    alpha=0.5
+)
+ax.yaxis._axinfo['grid'].update(
+    color='0.5',
+    linestyle='-',
+    linewidth=0.25,
+    alpha=0.5
+)
+ax.zaxis._axinfo['grid'].update(
+    color='0.5',
+    linestyle='-',
+    linewidth=0.25,
+    alpha=0.5
+)
+
+# Shorten the Z-axis (flatten effect)
+ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([1, 1, 0.55, 1]))
+ax.set_box_aspect(None, zoom=1.1)
+
+fig.savefig(
+    'data/snapshots/snapshots.png',
+    format='png',
+    dpi=400,
+    bbox_inches="tight",
+    transparent=False
+)
