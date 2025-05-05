1258db233b6a2273418ee55b3f867fd88e32ea2f
fpresenza <francisco.presenza@gmail.com>
2025-05-05 17:22:59 -0300

    load: minor improvements

--- uvnpy/network/load.py
+++ uvnpy/network/load.py
@@ -5,12 +5,12 @@ from numba import njit
 
 
 @njit
-def one_token_for_all(geodesics, extents):
+def one_token_for_all_sum(geodesics, extents):
     """
     Each node emits only one token that is broadcasted in order to reach
     all its subframework.
     """
-    load = 0
+    load = 0.0
     for i in np.nonzero(extents)[0]:
         n_broadcasters = np.sum(geodesics[i] < extents[i])
         load += n_broadcasters
@@ -19,14 +19,14 @@ def one_token_for_all(geodesics, extents):
 
 
 @njit
-def one_token_for_all_weighted(geodesics, extents):
+def one_token_for_all_weighted_sum(geodesics, extents):
     """
     Each node emits only one token that is broadcasted in order to reach
     all its subframework.
     The load is weighted using the size of the token wich is modeled as the
     size of the subframework minus the center.
     """
-    load = 0
+    load = 0.0
     for i in np.nonzero(extents)[0]:
         token_size = np.sum(geodesics[i] <= extents[i]) - 1
         n_broadcasters = np.sum(geodesics[i] < extents[i])
@@ -36,12 +36,12 @@ def one_token_for_all_weighted(geodesics, extents):
 
 
 @njit
-def one_token_for_each(geodesics, extents):
+def one_token_for_each_sum(geodesics, extents):
     """
     Each node emits one token for each other node in its subframework.
     Tokens are broadcasted by layer: 1-hop, 2-hops and so on.
     """
-    load = 0
+    load = 0.0
     for i in np.nonzero(extents)[0]:
         for h in range(1, extents[i] + 1):
             n_tokens = np.sum(geodesics[i] == h)
@@ -52,15 +52,17 @@ def one_token_for_each(geodesics, extents):
 
 
 @njit
-def one_token_for_each_per_node(geodesics, extent):
+def one_token_for_each(geodesics, extents):
     """
-    A node emits one token for each other node in its subframework.
+    Each node emits one token for each other node in its subframework.
     Tokens are broadcasted by layer: 1-hop, 2-hops and so on.
     """
-    load = 0
-    for h in range(1, extent + 1):
-        n_tokens = np.sum(geodesics == h)
-        n_broadcasters = np.sum(geodesics < h)
-        load += n_tokens * n_broadcasters
+    load = []
+    for i in np.nonzero(extents)[0]:
+        load.append(0.0)
+        for h in range(1, extents[i] + 1):
+            n_tokens = np.sum(geodesics[i] == h)
+            n_broadcasters = np.sum(geodesics[i] < h)
+            load[i] += n_tokens * n_broadcasters
 
     return load
