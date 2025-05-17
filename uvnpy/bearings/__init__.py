1f41a13193a86c4ccf02d15c09c936a8be67458c
fpresenza <francisco.presenza@gmail.com>
2025-05-17 20:20:37 -0300

    wip: __init__

--- uvnpy/bearings/__init__.py
+++ uvnpy/bearings/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from . import real_d            # noqa
-from . import se_2              # noqa
+from . import real_d     # noqa
+from . import real_d_s_1  # noqa
