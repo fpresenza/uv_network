f76f6cfa1d415e8ca3c7c59827bbf7a2639970f6
fpresenza <francisco.presenza@gmail.com>
2025-05-31 17:57:20 -0300

    join state dependent graphs and frameworks + minor fixes

--- uvnpy/network/graphs.py
+++ uvnpy/network/graphs.py
@@ -87,10 +87,10 @@ class Framework(Graph):
 
     def append_vertex(self, out_edges, in_edges, realization):
         super().append_vertex(out_edges, in_edges)
-        self._real = np.vstack([self.realization, realization])
+        self._real = np.vstack([self._real, realization])
 
 
-class DiskGraph(Graph):
+class DiskGraph(Framework):
     """
     Class representing a disk-like state dependant graph.
     """
@@ -98,13 +98,11 @@ class DiskGraph(Graph):
         """
         args:
         -----
-            realization : vertex positions in R^d
-            dmax : max connection range
+            realization : disk centers in some realization space
+            dmax : max connection distance
         """
         self.dmax = dmax
-        if realization is None:
-            self._adj = None
-        else:
+        if realization is not None:
             self.update(realization)
 
     def update(self, realization):
@@ -119,118 +117,60 @@ class DiskGraph(Graph):
         d = np.sqrt(np.square(r).sum(axis=-1))
         adj = d <= self.dmax
         adj[np.eye(len(realization), dtype=bool)] = False
-        super().update(adj)
+        super().update(adj, realization)
 
-
-class DiskFramework(DiskGraph):
-    """
-    Class representing a disk-like state dependant framework.
-    """
-    def __init__(self, position=None, dmax=np.inf):
-        """
-        args:
-        -----
-            position : disk centers in R^d
-            dmax : max conextion range
-        """
-        super().__init__(position, dmax)
-        if position is None:
-            self._pos = None
-        else:
-            self._pos = position.astype(float)
-
-    def position(self):
-        return self._pos.copy()
-
-    def update(self, position):
-        super().update(position)
-        self._pos = position
-
-    def append_vertex(self, position):
-        r = self._pos - position
+    def append_vertex(self, realization):
+        r = self._real - realization
         d = np.sqrt(np.square(r).sum(axis=-1))
         edges = d <= self.dmax
-        super().append_vertex(edges, edges)
-        self._pos = np.vstack([self._pos, position])
+        super().append_vertex(edges, edges, realization)
 
 
-class ConeGraph(Graph):
+class ConeGraph(Framework):
     """
-    Class representing a cone-like state dependant framework.
+    Class representing a cone-like state dependant graph.
     """
-    def __init__(self, positions=None, axes=None, dmax=np.inf, cmin=-1.0):
+    def __init__(self, realization=None, dmax=np.inf, cmin=-1.0):
         """
         args:
         -----
-            positions : cone apexes in R^d
-            axes : cone axes in R^d
-            dmax : max conextion range
+            realization : (n, 2*d) (cone apexes in R^d, cone axes in R^d)
+            dmax : max connection distance
             cmin : cosine of cone's half angle
         """
         self.dmax = dmax
         self.cmin = cmin
-        if (positions is None) or (axes is None):
-            self._adj = None
-        else:
-            self.update(positions, axes)
+        if realization is not None:
+            self.update(realization)
 
-    def update(self, positions, axes):
+    def update(self, realization):
         """
         Cone graph adjacency matrix.
 
         args:
         -----
-            positions : (n, d) vector array
-            axes      : (n, d) unit vector array
+            realization : (n, d) vector array
         """
+        positions = realization[:, :3]
+        axes = realization[:, 3:]
         r = positions - positions[:, np.newaxis]
         d = np.sqrt(np.square(r).sum(axis=-1))
         bearings = r / d[:, :, np.newaxis]
         cos = np.matmul(bearings, axes[:, :, np.newaxis]).squeeze()
         adj = np.logical_and(d <= self.dmax, cos >= self.cmin)
         adj[np.eye(len(positions), dtype=bool)] = False
-        super().update(adj)
+        super().update(adj, realization)
 
-
-class ConeFramework(ConeGraph):
-    """
-    Class representing a cone-like state dependant framework.
-    """
-    def __init__(self, positions, axes, dmax=np.inf, cmin=-1.0):
-        """
-        args:
-        -----
-            positions : cone apexes in R^d
-            axes : cone axes in R^d
-            dmax : max conextion range
-            cmin : cosine of cone's half angle
-        """
-        super().__init__(positions, axes, dmax, cmin)
-        self._pos = positions
-        self._axe = axes
-
-    def position(self):
-        return self._pos.copy()
-
-    def axes(self):
-        return self._axe.copy()
-
-    def update(self, positions, axes):
-        super().update(positions, axes)
-        self.pos = positions
-        self.axe = axes
-
-    def append_vertex(self, position, axe):
-        r = position - self._pos
+    def append_vertex(self, realization):
+        position = realization[:, :3]
+        axe = realization[:, 3:]
+        r = position - self._real[:, :3]
         d = np.sqrt(np.square(r).sum(axis=-1))
         in_bearings = r / d[:, np.newaxis]
-        in_cos = np.sum(in_bearings * self._axe, axis=-1)
+        in_cos = np.sum(in_bearings * self._real[:, 3:], axis=-1)
         out_cos = - np.sum(in_bearings * axe, axis=-1)
         in_ball = d <= self.dmax
 
         in_edges = np.logical_and(in_ball, in_cos >= self.cmin)
         out_edges = np.logical_and(in_ball, out_cos >= self.cmin)
-        super().append_vertex(out_edges, in_edges)
-
-        self._pos = np.vstack([self._pos, position])
-        self._axe = np.vstack([self._axe, axe])
+        super().append_vertex(out_edges, in_edges, realization)
