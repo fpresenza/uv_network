ac9329f19ac4b038c20d77216173cfa1ad9afba8
fpresenza <francisco.presenza@gmail.com>
2025-05-31 22:36:12 -0300

    new model for computing communication delay and complexity

--- tests/bearings/real_d/figs/dense_decomposition_metrics_cone.py
+++ tests/bearings/real_d/figs/dense_decomposition_metrics_cone.py
@@ -7,15 +7,12 @@ import argparse
 import progressbar
 from dataclasses import dataclass
 import numpy as np
-import transformations
 
 from uvnpy.toolkit.data import write_csv
-from uvnpy.network.core import (
-    geodesics, as_undirected, edges_from_adjacency
-)
+from uvnpy.network.core import geodesics, as_undirected
 from uvnpy.network.subframeworks import superframework_geodesics
 from uvnpy.bearings.real_d.core import is_inf_rigid, minimum_rigidity_extents
-from uvnpy.network import cone_graph
+from uvnpy.network.graphs import ConeGraph
 
 # ------------------------------------------------------------------
 # Definición de variables, funciones y clases
@@ -26,9 +23,7 @@ from uvnpy.network import cone_graph
 class Logs(object):
     nodes: np.ndarray
     delay: np.ndarray
-    delay_count: np.ndarray
     compl: np.ndarray
-    compl_count: np.ndarray
 
 
 # ------------------------------------------------------------------
@@ -36,53 +31,73 @@ class Logs(object):
 # ------------------------------------------------------------------
 
 
-def run(d, nmin, nmax, sens_range, rep, logs):
+def run(nmin, nmax, size, rep, logs):
     bar.start()
 
     sens_cos = np.cos(np.deg2rad(120.0 / 2))
+    sens_range = 1.0
 
-    for k, n in enumerate(range(nmin, nmax)):
-        bar.update(k)
-        logs.nodes[k] = n
+    for r in range(rep):
+        for k, n in enumerate(range(nmin, nmax)):
+            bar.update(size * r + k)
+            side_length = np.cbrt(n)  # side_length = np.cbrt(vol = n)
+            rigid_graph = False
 
-        delay = np.array([], dtype=int)
-        compl = np.array([], dtype=float)
-        r = 0
-
-        while r < rep:
-            p = np.random.uniform(0.0, 1.0, (n, d))
-            baricenter = np.mean(p, axis=0)
-            axes = transformations.unit_vector(baricenter - p, axis=1)
-            D = cone_graph.adjacency_matrix(p, axes, sens_range, sens_cos)
-            E = edges_from_adjacency(D, directed=True)
-            A = as_undirected(D).astype(float)
-            if is_inf_rigid(E, p):
-                G = geodesics(A)
-                h = minimum_rigidity_extents(G, p)
-                s = superframework_geodesics(G, h)
-                n_state = [
-                    len([j for j in range(n) if G[i, j] < s[j]])
-                    for i in range(n)
-                ]
-                n_action = [
-                    sum(
-                        len([k for k in range(n) if G[i, j] < G[j, k] <= h[j]])
-                        for j in range(n)
+            while not rigid_graph:
+                if n == nmin:
+                    positions = np.random.uniform(0.0, side_length, (nmin, 3))
+                    angles = np.random.uniform(-np.pi, np.pi, nmin)
+                    axes = np.empty((nmin, 3), dtype=float)
+                    axes[:nmin, 0] = np.cos(angles)
+                    axes[:nmin, 1] = np.sin(angles)
+                    axes[:nmin, 2] = 0.0
+                    cone_graph = ConeGraph(
+                        positions, axes, dmax=sens_range, cmin=sens_cos
                     )
-                    for i in range(n)
-                ]
-                delay_ratio = 2 * h / G.max()
-                compl_ratio = np.add(n_state, n_action) / (1 + A.sum(axis=1))
-                delay = np.append(delay, delay_ratio)
-                compl = np.append(compl, compl_ratio)
-                r += 1
+                else:
+                    position = np.random.uniform(0.0, side_length, 3)
+                    angle = np.random.uniform(-np.pi, np.pi)
+                    axis = np.empty(3, dtype=float)
+                    axis[0] = np.cos(angle)
+                    axis[1] = np.sin(angle)
+                    axis[2] = 0.0
+                    cone_graph.append_vertex(position, axis)
 
-        logs.delay[k], logs.delay_count[k] = np.unique(
-            delay, return_counts=True
-        )
-        logs.compl[k], logs.compl_count[k] = np.unique(
-            compl, return_counts=True
-        )
+                E = cone_graph.edge_set()
+                p = cone_graph.positions(d=3)
+                if is_inf_rigid(E, p):
+                    rigid_graph = True
+                    A = as_undirected(
+                        cone_graph.adjacency_matrix()
+                    ).astype(float)
+                    G = geodesics(A)
+                    h = minimum_rigidity_extents(G, p)
+                    s = superframework_geodesics(G, h)
+                    n_state = np.sum([
+                        len([j for j in range(n) if G[i, j] < s[j]])
+                        for i in range(n)
+                    ])
+                    n_action = np.sum([
+                        sum(
+                            len([
+                                k for k in range(n)
+                                if G[i, j] < G[j, k] <= h[j]
+                            ])
+                            for j in range(n)
+                        )
+                        for i in range(n)
+                    ])
+                    d_comm = np.max([
+                        max([
+                            h[j] + G[i, j] for j in range(n)
+                            if G[i, j] <= h[j]
+                        ])
+                        for i in range(n)
+                    ])
+                    logs.delay[r, k] = d_comm / G.max()
+                    logs.compl[r, k] = (n_state + n_action) / A.sum()
+                else:
+                    cone_graph.remove_vertex(-1)
 
     bar.finish()
     return logs
@@ -97,10 +112,6 @@ parser.add_argument(
     default=50, type=int, help='number of nodes'
 )
 parser.add_argument(
-    '-s', '--sens_range',
-    default=1.0, type=float, help='sensing range'
-)
-parser.add_argument(
     '-r', '--rep',
     default=1, type=int, help='number of repetitions'
 )
@@ -110,29 +121,23 @@ arg = parser.parse_args()
 # ------------------------------------------------------------------
 # Configuración
 # ------------------------------------------------------------------
-d = 3
-sens_range = arg.sens_range
 nmin = 2
 nmax = arg.nodes + 1
 size = nmax - nmin
 rep = arg.rep
 logs = Logs(
-    nodes=np.empty(size, dtype=int),
-    delay=np.empty(size, dtype=np.ndarray),
-    delay_count=np.empty(size, dtype=np.ndarray),
-    compl=np.empty(size, dtype=np.ndarray),
-    compl_count=np.empty(size, dtype=np.ndarray)
+    nodes=np.arange(nmin, nmax),
+    delay=np.empty((rep, size), dtype=np.ndarray),
+    compl=np.empty((rep, size), dtype=np.ndarray),
 )
 
 # ------------------------------------------------------------------
 # Simulación
 # ------------------------------------------------------------------
-bar = progressbar.ProgressBar(maxval=size)
+bar = progressbar.ProgressBar(maxval=size * rep)
 
-logs = run(d, nmin, nmax, sens_range, rep, logs)
+logs = run(nmin, nmax, size, rep, logs)
 
 write_csv('/tmp/nodes.csv', logs.nodes, one_row=True)
 write_csv('/tmp/delay.csv', logs.delay)
-write_csv('/tmp/delay_count.csv', logs.delay_count)
 write_csv('/tmp/compl.csv', logs.compl)
-write_csv('/tmp/compl_count.csv', logs.compl_count)
--- tests/bearings/real_d/figs/dense_decomposition_metrics_cone_plot.py
+++ tests/bearings/real_d/figs/dense_decomposition_metrics_cone_plot.py
@@ -24,9 +24,7 @@ plt.rcParams['font.family'] = 'serif'
 # ------------------------------------------------------------------
 nodes = np.loadtxt('/tmp/nodes.csv', delimiter=',')
 delay = read_csv('/tmp/delay.csv', rows=(0, np.inf), dtype=float)
-delay_count = read_csv('/tmp/delay_count.csv', rows=(0, np.inf), dtype=int)
 compl = read_csv('/tmp/compl.csv', rows=(0, np.inf), dtype=float)
-compl_count = read_csv('/tmp/compl_count.csv', rows=(0, np.inf), dtype=int)
 
 # ------------------------------------------------------------------
 # Plotting
@@ -43,43 +41,27 @@ ax.tick_params(
     labelsize='x-small'
 )
 ax.grid(lw=0.4)
-ax.set_ylim(0.0, 100.0)
 ax.set_xlabel(r'$n$', fontsize=8)
-# ax.set_ylabel(r'(%)', fontsize=8)
-half = np.array([], dtype=float)
-whole = np.array([], dtype=float)
-
-for u, c in zip(delay, delay_count):
-    s = c.sum()
-    half = np.append(half, c[u <= 0.5].sum() * 100 / s)
-    whole = np.append(whole, c[u <= 1.0].sum() * 100 / s)
-
+ax.set_ylabel(r'Delay', fontsize=8)
 ax.plot(
-    nodes, half,
-    label=r'$(h_i \leq 0.5) \, \%$', lw=0.5,
+    nodes, np.median(delay, axis=0),
+    label=r'$\sigma = 1$', lw=1,
     color='C0',
     marker='o', markersize=3, markevery=10
 )
 ax.fill_between(
-    nodes, 0.0, half,
-    color='C0', alpha=0.3
-)
-ax.plot(
-    nodes, whole,
-    label=r'$(h_i \leq 1.0) \, \%$', lw=0.5,
-    color='C1',
-    marker='s', markersize=3, markevery=10
-)
-ax.fill_between(
-    nodes, half, whole,
-    color='C1', alpha=0.3
+    nodes,
+    np.quantile(delay, q=0.25, axis=0),
+    np.quantile(delay, q=0.75, axis=0),
+    alpha=0.3,
+    color='C0',
+    # marker='o', markersize=3, markevery=10
 )
 ax.legend(
     fontsize='x-small', handlelength=1,
     labelspacing=0.3, borderpad=0.2, loc='lower right'
 )
 fig.savefig('/tmp/bearing_delay.png', format='png', dpi=600)
-
 
 # ------------------------------------------------------------------
 # Complexity
@@ -93,48 +75,22 @@ ax.tick_params(
     labelsize='x-small'
 )
 ax.grid(lw=0.4)
-ax.set_ylim(0.0, 100.0)
 ax.set_xlabel(r'$n$', fontsize=8)
-# ax.set_ylabel(r'(%)', fontsize=8)
-exp_2 = np.array([], dtype=float)
-exp_4 = np.array([], dtype=float)
-exp_8 = np.array([], dtype=float)
-
-for u, c in zip(compl, compl_count):
-    s = c.sum()
-    exp_2 = np.append(exp_2, c[u <= 2].sum() * 100 / s)
-    exp_4 = np.append(exp_4, c[u <= 4].sum() * 100 / s)
-    exp_8 = np.append(exp_8, c[u <= 8].sum() * 100 / s)
+ax.set_ylabel(r'Complexity', fontsize=8)
 
 ax.plot(
-    nodes, exp_2,
-    label=r'$(c_i \leq 2) \, \%$', lw=0.5,
+    nodes, np.median(compl, axis=0),
+    label=r'$\sigma = 1$', lw=1,
     color='C0',
     marker='o', markersize=3, markevery=10
 )
 ax.fill_between(
-    nodes, 0.0, exp_2,
-    color='C0', alpha=0.3
-)
-ax.plot(
-    nodes, exp_4,
-    label=r'$(c_i \leq 4) \, \%$', lw=0.5,
-    color='C1',
-    marker='s', markersize=3, markevery=10
-)
-ax.fill_between(
-    nodes, exp_2, exp_4,
-    color='C1', alpha=0.3
-)
-ax.plot(
-    nodes, exp_8,
-    label=r'$(c_i \leq 8) \, \%$', lw=0.5,
-    color='C2',
-    marker='x', markersize=3, markevery=10
-)
-ax.fill_between(
-    nodes, exp_4, exp_8,
-    color='C2', alpha=0.3
+    nodes,
+    np.quantile(compl, q=0.25, axis=0),
+    np.quantile(compl, q=0.75, axis=0),
+    alpha=0.3,
+    color='C0',
+    # marker='o', markersize=3, markevery=10
 )
 ax.legend(
     fontsize='x-small', handlelength=1,
