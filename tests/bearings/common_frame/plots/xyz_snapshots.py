6ab440813e69409d825fd825e3fc0ea350a696bb
fpresenza <francisco.presenza@gmail.com>
2025-11-25 18:28:59 -0300

    create snapshots frames from two pov

--- tests/bearings/common_frame/plots/xyz_snapshots.py
+++ tests/bearings/common_frame/plots/xyz_snapshots.py
@@ -83,38 +83,51 @@ bar = progressbar.ProgressBar(maxval=N).start()
 
 for k in range(N):
     tk = t[k]
-    fig = plt.figure(figsize=(5, 5))
-    ax = fig.add_subplot(projection="3d")
-    # fig.subplots_adjust(right=0.5)
+    fig, axes = plt.subplots(
+        1, 2, subplot_kw={"projection": "3d"}, figsize=(12, 6)
+    )
+    fig.subplots_adjust(bottom=0.0, wspace=0.0, right=1.0, top=1.0)
     # fig.set_size_inches(10, 8)  # Width = 10, Height = 8
     # fig.tight_layout()
-    ax.tick_params(
-        axis='both',       # changes apply to the x-axis
-        which='both',      # both major and minor ticks are affected
-        pad=-2,
-        labelsize='x-small'
+
+    for ax in axes:
+        ax.tick_params(
+            axis='both',       # changes apply to the x-axis
+            which='both',      # both major and minor ticks are affected
+            pad=-2,
+            labelsize='small'
+        )
+        ax.set_aspect('equal')
+        ax.set_xlabel(r'$x \ (\mathrm{m})$', fontsize='small', labelpad=0.5)
+        ax.set_ylabel(r'$y \ (\mathrm{m})$', fontsize='small', labelpad=0.5)
+        ax.set_zlabel(r'$z \ (\mathrm{m})$', fontsize='small', labelpad=-8.0)
+        # ax.zaxis.labelpad = 0
+
+        ax.set_xlim3d(0, 100.0)
+        ax.set_ylim3d(0, 100.0)
+        ax.set_zlim3d(0.0, 50.0)
+        ax.set_xticks([0.0, 25.0, 50.0, 75.0, 100.0])
+        ax.set_yticks([0.0, 25.0, 50.0, 75.0, 100.0])
+        ax.set_zticks([0.0, 25.0, 50.0])
+
+    axes[0].view_init(elev=20.0, azim=-70.0)
+    # Shorten the Z-axis (flatten effect)
+    axes[0].get_proj = lambda: np.dot(
+        Axes3D.get_proj(axes[0]), np.diag([1, 1, 0.55, 1])
     )
-    ax.set_aspect('equal')
-    ax.set_xlabel(r'$x \ (\mathrm{m})$', fontsize='small', labelpad=0.5)
-    ax.set_ylabel(r'$y \ (\mathrm{m})$', fontsize='small', labelpad=0.5)
-    ax.set_zlabel(r'$z \ (\mathrm{m})$', fontsize='small', labelpad=-8.0)
-    # ax.zaxis.labelpad = 0
+    axes[0].set_box_aspect(None, zoom=1.0)
 
-    ax.set_xlim3d(0, 100.0)
-    ax.set_ylim3d(0, 100.0)
-    ax.set_zlim3d(0.0, 50.0)
-    ax.set_xticks([0.0, 25.0, 50.0, 75.0, 100.0])
-    ax.set_yticks([0.0, 25.0, 50.0, 75.0, 100.0])
-    ax.set_zticks([0.0, 25.0, 50.0])
-    # ax.set_xticklabels([])
-    # ax.set_yticklabels([])
-    # ax.set_zticklabels([])
-    ax.view_init(elev=40.0, azim=-30.0)
+    axes[1].view_init(elev=20.0, azim=-10.0)
+    # Shorten the Z-axis (flatten effect)
+    axes[1].get_proj = lambda: np.dot(
+        Axes3D.get_proj(axes[1]), np.diag([1, 1, 0.55, 1])
+    )
+    axes[1].set_box_aspect(None, zoom=1.0)
 
-    ax.text(
+    axes[0].text(
         120.0, 100.0, 150.0, r't = {:.3f}s'.format(tk),
         verticalalignment='bottom', horizontalalignment='left',
-        transform=ax.transAxes, color='g', fontsize=8
+        transform=axes[0].transAxes, color='g', fontsize=10
     )
 
     p = x[k][:, :3]
@@ -122,77 +135,76 @@ for k in range(N):
     for i in range(n):
         axis = np.array([np.cos(psi[i]), np.sin(psi[i]), 0.0])
         cone = geometry.cone(p[i], axis, 3.0, np.pi/3)
-        ax.add_collection3d(art3d.Poly3DCollection(cone, alpha=0.5))
-
-    plot.points(
-        ax, p,
-        facecolor='b',
-        edgecolor='none',
-        marker='o',
-        s=10,
-        # lw=1,
-        zorder=10,
-        # alpha=1
-    )
-    plot.bars(
-        ax,
-        p,
-        core.edges_from_adjacency(A[k]),
-        color='0.0',
-        alpha=0.5,
-        lw=0.5,
-        zorder=0
-    )
+        axes[0].add_collection3d(art3d.Poly3DCollection(cone, alpha=0.5))
+        axes[1].add_collection3d(art3d.Poly3DCollection(cone, alpha=0.5))
 
     untracked = targets[k][:, 3].astype(bool)
     tracked = np.logical_not(untracked)
-    ax.scatter(
-        targets[k][untracked, 0],
-        targets[k][untracked, 1],
-        targets[k][untracked, 2],
-        marker='d',
-        # linewidth=2,
-        edgecolor='black',
-        facecolor='black',
-        s=6,
-    )
-    ax.scatter(
-        targets[k][tracked, 0],
-        targets[k][tracked, 1],
-        targets[k][tracked, 2],
-        marker='d',
-        linewidth=0.2,
-        edgecolor='black',
-        facecolor='none',
-        s=3,
-    )
-    ax.xaxis._axinfo['grid'].update(
-        color='0.5',
-        linestyle='-',
-        linewidth=0.25,
-        alpha=0.5
-    )
-    ax.yaxis._axinfo['grid'].update(
-        color='0.5',
-        linestyle='-',
-        linewidth=0.25,
-        alpha=0.5
-    )
-    ax.zaxis._axinfo['grid'].update(
-        color='0.5',
-        linestyle='-',
-        linewidth=0.25,
-        alpha=0.5
-    )
 
-    # Shorten the Z-axis (flatten effect)
-    ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([1, 1, 0.55, 1]))
-    ax.set_box_aspect(None, zoom=1.0)
+    for ax in axes:
+        plot.points(
+            ax, p,
+            facecolor='b',
+            edgecolor='none',
+            marker='o',
+            s=10,
+            # lw=1,
+            zorder=10,
+            # alpha=1
+        )
+        plot.bars(
+            ax,
+            p,
+            core.edges_from_adjacency(A[k]),
+            color='0.0',
+            alpha=0.5,
+            lw=0.5,
+            zorder=0
+        )
+
+        ax.scatter(
+            targets[k][untracked, 0],
+            targets[k][untracked, 1],
+            targets[k][untracked, 2],
+            marker='d',
+            # linewidth=2,
+            edgecolor='black',
+            facecolor='black',
+            s=6,
+        )
+        ax.scatter(
+            targets[k][tracked, 0],
+            targets[k][tracked, 1],
+            targets[k][tracked, 2],
+            marker='d',
+            linewidth=0.2,
+            edgecolor='black',
+            facecolor='none',
+            s=3,
+        )
+        ax.xaxis._axinfo['grid'].update(
+            color='0.5',
+            linestyle='-',
+            linewidth=0.25,
+            alpha=0.5
+        )
+        ax.yaxis._axinfo['grid'].update(
+            color='0.5',
+            linestyle='-',
+            linewidth=0.25,
+            alpha=0.5
+        )
+        ax.zaxis._axinfo['grid'].update(
+            color='0.5',
+            linestyle='-',
+            linewidth=0.25,
+            alpha=0.5
+        )
 
     fig.savefig(
         'xyz_snapshots/frame{}.png'.format(str(k + k_i_jump).zfill(3)),
         format='png',
-        dpi=200,
+        dpi=300,
         bbox_inches="tight",
         # transparent=True
     )
