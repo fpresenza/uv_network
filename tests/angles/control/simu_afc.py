6263d05b1f4c63d10657809ec4cc34afe1f45904
fpresenza <francisco.presenza@gmail.com>
2026-02-22 19:24:01 -0300

    wip afc: split pose

--- tests/angles/control/simu_afc.py
+++ tests/angles/control/simu_afc.py
@@ -5,10 +5,12 @@ import argparse
 from dataclasses import dataclass
 import progressbar
 import numpy as np
+# from transformations import unit_vector
 
 from uvnpy.graphs.core import adjacency_matrix_from_edges
 from uvnpy.dynamics.core import EulerIntegrator
 from uvnpy.toolkit.geometry import rotation_matrix_from_quaternion
+from uvnpy.angles.local_frame.core import angle_indices, angle_function
 
 
 # ------------------------------------------------------------------
@@ -23,12 +25,13 @@ np.set_printoptions(
 @dataclass
 class Logs(object):
     time: list
-    pose: list
+    position: list
+    orientation: list
     adjacency: list
 
 
-def random_position(lim):
-    return np.random.uniform(-lim, lim, (3,))
+def random_position(*args, **kwargs):
+    return np.random.uniform(*args, **kwargs)
 
 
 def random_rotation_matrix():
@@ -37,8 +40,8 @@ def random_rotation_matrix():
     return rotation_matrix_from_quaternion(q)
 
 
-def stack_pose(pose):
-    return np.hstack([np.hstack([p.x(), R.x().ravel()]) for p, R in pose])
+def extract(integrators):
+    return [p.x().ravel() for p in integrators]
 
 
 # ------------------------------------------------------------------
@@ -49,14 +52,29 @@ def stack_pose(pose):
 def simu_step():
     """Formation control algorithm"""
     for i in nodes:
-        pass
+        # --- measurements --- #
+        # out_neighbors = edge_set[:, 1][edge_set[:, 0] == i]
+        # bearings = [unit_vector(p[j] - p[i]) for j in out_neighbors]
+
+        # --- control law --- #
+        # kp, kd = 1.0, 1.0
+        u = np.zeros(3, dtype=np.float64)
+        # out_angles = angle_set[angle_set[:, 0] == i]
+        # for angle in out_angles:
+        #     u += 0.0
+        # in_angles = angle_set[np.logical_or(
+        #     angle_set[:, 1] == i, angle_set[:, 2] == i
+        # )]
+        # for angle in in_angles:
+        #     u += 0.0
+        p_int[i].step(t, u)
 
 
 def log_step():
     """Data log"""
     if (simu_counter % log_skip == 0):
         logs.time.append(t)
-        logs.pose.append(stack_pose(pose))
+        logs.position.append(np.hstack(extract(p_int)))
 
 
 # ------------------------------------------------------------------
@@ -84,6 +102,7 @@ arg = parser.parse_args()
 # Simulation parameters
 simu_length = arg.simu_length
 simu_step_size = arg.simu_step_size
+simu_step_size_sec = simu_step_size * 1e-3
 
 if simu_length % simu_step_size != 0:
     print('\
@@ -92,41 +111,47 @@ if simu_length % simu_step_size != 0:
     ')
 
 simu_step_num = int(simu_length / simu_step_size)
-# time_steps = [simu_step_size * k for k in range(simu_step_num)]
 log_skip = arg.log_skip
 
-# np.random.seed(6)
+np.random.seed(0)
 
 print(
     'Simulation Time: begin = {} sec, end = {} sec, step = {} sec'
-    .format(0.0, simu_length, simu_step_size)
+    .format(0.0, simu_length * 1e-3, simu_step_size_sec)
 )
 
-# world parameters
+# World parameters
 t = 0.0
 n = 3
 nodes = np.arange(n)
 edge_set = np.array([
     [0, 1],
+    [0, 2],
+    [1, 0],
     [1, 2],
 ])
-pose = [
-    (
-        EulerIntegrator(random_position(1.0)),
-        EulerIntegrator(random_rotation_matrix())
-    )
+angle_set = angle_indices(n, edge_set).astype(int)
+print(angle_set)
+
+desired_position = random_position(0.0, 1.0, (n, 3))
+desired_angles = angle_function(edge_set, desired_position)
+# print(desired_angles)
+
+p_int = [
+    EulerIntegrator(desired_position[i] + random_position(0.0, 0.0, (3,)))
     for i in nodes
 ]
+o_int = [EulerIntegrator(random_rotation_matrix()) for _ in nodes]
 
-for (p, R) in pose:
-    print(p.x())
-    print(R.x())
-    print('\n')
+angles = angle_function(edge_set, np.vstack(extract(p_int)))
+# print(angles)
+
 
 # initialize logs
 logs = Logs(
     time=[t],
-    pose=[stack_pose(pose)],
+    position=[np.hstack(extract(p_int))],
+    orientation=[np.hstack(extract(o_int))],
     adjacency=adjacency_matrix_from_edges(n, edge_set)
 )
 
@@ -138,7 +163,7 @@ simu_counter = 1
 bar = progressbar.ProgressBar(maxval=arg.simu_length).start()
 
 while simu_counter <= simu_step_num:
-    t += simu_step_size
+    t += simu_step_size_sec
 
     simu_step()
     log_step()
@@ -150,4 +175,5 @@ while simu_counter <= simu_step_num:
 bar.finish()
 
 np.savetxt('simu_data/t.csv', logs.time, delimiter=',')
-np.savetxt('simu_data/pose.csv', logs.pose, delimiter=',')
+np.savetxt('simu_data/position.csv', logs.position, delimiter=',')
+np.savetxt('simu_data/orientation.csv', logs.orientation, delimiter=',')
