c3c707af86c533e7a997a79489c5da7d666fa659
fpresenza <francisco.presenza@gmail.com>
2026-02-23 19:14:44 -0300

    new: integrators for matrix lie groups

---
+++ uvnpy/dynamics/lie_groups.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author Francisco Presenza
+@institute LAR - FIUBA, Universidad de Buenos Aires, Argentina
+"""
+import numpy as np
+import scipy.linalg
+
+from uvnpy.dynamics.core import EulerIntegrator
+from uvnpy.toolkit.geometry import rotation_matrix_from_vector
+
+
+class EulerIntegratorLieGroup(EulerIntegrator):
+    def exp_map(self, h):
+        return scipy.linalg.expm(h)
+
+    def step(self, t, u):
+        dt = t - self.t
+        self._x = self.exp_map(dt * self._u).dot(self._x)
+        self.t = t
+        self._u = u
+
+
+class EulerIntegratorOrtogonalGroup(EulerIntegratorLieGroup):
+    def exp_map(self, h):
+        if np.allclose(h, 0.0):
+            dx = np.eye(3)
+        else:
+            dx = rotation_matrix_from_vector(h[[2, 0, 1], [1, 2, 0]])
+        return dx
