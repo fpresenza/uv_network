6bd21c11a44116f864eab9b893c7058ece1df430
fpresenza <francisco.presenza@gmail.com>
2026-02-24 15:03:53 -0300

    wip: angle network localization

---
+++ tests/angles/estimation/simu_anl.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+from dataclasses import dataclass
+import progressbar
+import numpy as np
+from transformations import unit_vector
+
+from uvnpy.graphs.core import adjacency_matrix_from_edges
+from uvnpy.dynamics.core import EulerIntegrator
+from uvnpy.toolkit.geometry import rotation_matrix_from_quaternion
+from uvnpy.angles.local_frame.core import (
+    angle_indices,
+    angle_function,
+    is_angle_rigid
+)
+
+# ------------------------------------------------------------------
+# Functions, Classes and Configurations
+# ------------------------------------------------------------------
+np.set_printoptions(suppress=True, precision=10)
+
+
+@dataclass
+class Logs(object):
+    time: list
+    position: list
+    estimated_position: list
+    adjacency: list
+
+
+def random_rotation_matrix():
+    q = np.random.normal(size=4)
+    q /= np.sqrt(q.dot(q))
+    return rotation_matrix_from_quaternion(q)
+
+
+def extract(integrators, wrapper=lambda x: x):
+    return [wrapper(p.x()) for p in integrators]
+
+
+def complete_angle_set(out_neighbors):
+    i, j = np.triu_indices(out_neighbors.size, k=1)
+    return np.column_stack([out_neighbors[i], out_neighbors[j]])
+
+
+# ------------------------------------------------------------------
+# Simulation loop inner functions
+# ------------------------------------------------------------------
+
+
+def simu_step():
+    """Formation control algorithm"""
+    # --- data ---#
+    p = extract(p_int)
+    u = np.zeros((n, 3), dtype=np.float64)
+
+    for i in nodes:
+        out_neighbors = edge_set[:, 1][edge_set[:, 0] == i]
+
+        # --- estimated values --- #
+        distances = {
+            j: np.sqrt(np.sum((p[j] - p[i])**2)) for j in out_neighbors
+        }
+        bearings = {j: unit_vector(p[j] - p[i]) for j in out_neighbors}
+
+        # --- control law --- #
+        for j, k in complete_angle_set(out_neighbors):
+            dij = distances[j]
+            bij = bearings[j]
+            Pij = np.eye(3) - np.outer(bij, bij)
+
+            dik = distances[k]
+            bik = bearings[k]
+            Pik = np.eye(3) - np.outer(bik, bik)
+
+            ijk = np.all(angle_set == (i, j, k), axis=1)
+            eijk = bij.dot(bik) - measured_angles[ijk]
+            qijk = Pij.dot(bik) / dij
+            qikj = Pik.dot(bij) / dik
+
+            u[i] += eijk * (qijk + qikj)
+            u[j] -= eijk * qijk
+            u[k] -= eijk * qikj
+
+    for i in nodes:
+        p_int[i].step(t, u[i])
+
+
+def log_step():
+    """Data log"""
+    logs.time.append(t)
+    logs.estimated_position.append(np.hstack(extract(p_int)))
+
+
+# ------------------------------------------------------------------
+# Argument parse
+# ------------------------------------------------------------------
+parser = argparse.ArgumentParser(description='')
+parser.add_argument(
+    '-s', '--simu_step_size',
+    default=1, type=int, help='simulation step in milli seconds'
+)
+parser.add_argument(
+    '-t', '--simu_length',
+    default=1, type=int, help='total simulation time in milli seconds'
+)
+parser.add_argument(
+    '-l', '--log_skip',
+    default=1, type=int, help='logger skip in number of simu_step_size'
+)
+arg = parser.parse_args()
+
+# ------------------------------------------------------------------
+# Configuration
+# ------------------------------------------------------------------
+# --- simulation parameters --- #
+if arg.simu_length % arg.simu_step_size != 0:
+    print('\
+        Simulation length is not a multiple of the step size. \
+        Length will be truncated the closest multiple.\
+    ')
+simu_num_steps = int(arg.simu_length / arg.simu_step_size)
+
+simu_length = arg.simu_length * 1e-3    # in seconds
+simu_step_size = arg.simu_step_size * 1e-3    # in seconds
+log_skip = arg.log_skip
+
+np.random.seed(0)
+
+print(
+    'Simulation Time: begin = {} sec, end = {} sec, step = {} sec'
+    .format(0.0, simu_length, simu_step_size)
+)
+print(
+    'Logging Time: begin = {} sec, end = {} sec, step = {} sec'
+    .format(0.0, simu_length, simu_step_size * log_skip)
+)
+
+# --- world parameters --- #
+t = 0.0
+n = 4
+nodes = np.arange(n)
+edge_set = np.array([
+    [0, 1],
+    [0, 2],
+    [1, 0],
+    [1, 2],
+    [0, 3],
+    [1, 3]
+])
+angle_set = angle_indices(n, edge_set).astype(int)
+# print(angle_set)
+
+position = np.random.uniform(0.0, 1.0, (n, 3))
+measured_angles = angle_function(edge_set, position)
+# print(measured_angles)
+
+if not is_angle_rigid(edge_set, position):
+    raise ValueError('The framework is not IAR.')
+
+p_int = [
+    EulerIntegrator(np.random.normal(position[i], 0.1, size=3))
+    for i in nodes
+]
+
+# initialize logs
+logs = Logs(
+    time=[t],
+    position=[position.ravel()],
+    estimated_position=[np.hstack(extract(p_int))],
+    adjacency=[adjacency_matrix_from_edges(n, edge_set).ravel()]
+)
+# print(logs.position[0])
+# print(logs.orientation[0])
+
+
+# ------------------------------------------------------------------
+# Simulation
+# ------------------------------------------------------------------
+
+simu_counter = 1
+bar = progressbar.ProgressBar(maxval=simu_length).start()
+
+while simu_counter < simu_num_steps:
+    t += simu_step_size
+
+    simu_step()
+    if (simu_counter % log_skip == 0):
+        log_step()
+
+    simu_counter += 1
+
+    bar.update(np.round(t, 3))
+
+bar.finish()
+
+np.savetxt('simu_data/t.csv', logs.time, delimiter=',')
+np.savetxt('simu_data/position.csv', logs.position, delimiter=',')
+np.savetxt(
+    'simu_data/estimated_position.csv', logs.estimated_position, delimiter=','
+)
+np.savetxt('simu_data/adjacency.csv', logs.adjacency, delimiter=',')
---
+++ tests/angles/estimation/time_plots.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+
+from uvnpy.toolkit.data import read_csv_numpy
+from uvnpy.angles.local_frame.core import angle_function
+from uvnpy.graphs.core import edges_from_adjacency
+
+plt.rcParams['text.usetex'] = False
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
+plt.rcParams['mathtext.fontset'] = 'dejavuserif'
+plt.rcParams['font.family'] = 'serif'
+
+
+# ------------------------------------------------------------------
+# Read simulated data
+# ------------------------------------------------------------------
+t = read_csv_numpy('simu_data/t.csv')
+log_num_steps = len(t)
+
+estimated_position = read_csv_numpy(
+    'simu_data/estimated_position.csv'
+).reshape(log_num_steps, -1, 3)
+n = len(estimated_position[0])
+position = read_csv_numpy(
+    'simu_data/position.csv'
+).reshape(-1, 3)
+adjacency = read_csv_numpy('simu_data/adjacency.csv').reshape(n, n)
+
+edge_set = edges_from_adjacency(adjacency)
+
+angles = angle_function(edge_set, position)
+angles = [angles for _ in t]
+
+# ------------------------------------------------------------------
+# Plot positions
+# ------------------------------------------------------------------
+fig, ax = plt.subplots(3, 1, figsize=(9.0, 6.0))
+fig.subplots_adjust(
+    bottom=0.215,
+    top=0.925,
+    wspace=0.33,
+    right=0.975,
+    left=0.18
+)
+
+for k, d in enumerate(['x', 'y', 'z']):
+    ax[k].tick_params(
+        axis='both',       # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        pad=1,
+        labelsize=9
+    )
+
+    ax[k].set_xlabel(r'$t\ (\mathrm{s})$', fontsize=10)
+    ax[k].set_ylabel(fr'$\hat{{p}}_{{i, {d}}} \ (\rm m)$', fontsize=10)
+    ax[k].set_ylim(-1.5, 1.5)
+    ax[k].grid(1)
+
+    ax[k].plot(t, estimated_position[:, :, k], lw=1.0, ds='steps-post')
+
+fig.savefig('time_plots/estimated_position.pdf', bbox_inches='tight')
+
+# ------------------------------------------------------------------
+# Plot angles
+# ------------------------------------------------------------------
+fig, ax = plt.subplots(figsize=(9.0, 6.0))
+fig.subplots_adjust(
+    bottom=0.215,
+    top=0.925,
+    wspace=0.33,
+    right=0.975,
+    left=0.18
+)
+
+ax.tick_params(
+    axis='both',       # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    pad=1,
+    labelsize=9
+)
+
+ax.set_xlabel(r'$t\ (\mathrm{s})$', fontsize=10)
+ax.set_ylabel(r'$a_{ijk}$', fontsize=10)
+ax.set_ylim(-1.0, 1.0)
+ax.grid(1)
+
+ax.plot(
+    t,
+    [angle_function(edge_set, p) for p in estimated_position],
+    lw=1.0, ds='steps-post'
+)
+ax.plot([], [], color='k', label='estimated')
+ax.set_prop_cycle(None)    # resets color counter
+ax.plot(t, angles, lw=0.8, ls='--')
+ax.plot([], [], color='k', ls='--', label='real')
+ax.legend(
+    fontsize=10, handlelength=1.5, labelspacing=0.4,
+    borderpad=0.2, handletextpad=0.2, framealpha=1.,
+    ncol=2, columnspacing=1
+)
+
+fig.savefig('time_plots/angles.pdf', bbox_inches='tight')
+
+# ------------------------------------------------------------------
+# Plot angle errors
+# ------------------------------------------------------------------
+fig, ax = plt.subplots(figsize=(9.0, 6.0))
+fig.subplots_adjust(
+    bottom=0.215,
+    top=0.925,
+    wspace=0.33,
+    right=0.975,
+    left=0.18
+)
+
+ax.tick_params(
+    axis='both',       # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    pad=1,
+    labelsize=9
+)
+
+ax.set_xlabel(r'$t\ (\mathrm{s})$', fontsize=10)
+ax.set_ylabel(r'$|\hat{a}_{ijk} - a_{ijk}|$', fontsize=10)
+ax.set_ylim(0.0, None)
+ax.grid(1)
+
+ax.plot(
+    t,
+    [
+        np.abs(angle_function(edge_set, estimated_position[k]) - angles[k])
+        for k in range(log_num_steps)
+    ],
+    lw=1.0, ds='steps-post'
+)
+fig.savefig('time_plots/angle_errors.pdf', bbox_inches='tight')
+
+plt.show()
