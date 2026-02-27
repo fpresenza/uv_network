5172f04b2efbb8c1737f33c6be6ff29556a27c76
fpresenza <francisco.presenza@gmail.com>
2026-02-27 15:30:02 -0300

    weights gradient w.r.t. orientation

--- tests/angles/arm/weights_gradient.py
+++ tests/angles/arm/weights_gradient.py
@@ -5,14 +5,22 @@ import numpy as np
 from transformations import unit_vector
 
 from uvnpy.toolkit.functions import cosine_activation, cosine_activation_derivative
+from uvnpy.toolkit.geometry import (
+    cross_product_matrix,
+    rotation_matrix_from_vector
+)
 
 # ------------------------------------------------------------------
 # Functions, Classes and Configurations
 # ------------------------------------------------------------------
 
 
+def cross_product_matrix_inverse(S):
+    return np.array([S[2, 1], S[0, 2], S[1, 0]])
+
+
 def w(p, R):
-    ei = R[0][:, 0]
+    ei = R[:, 0]
 
     dij = np.linalg.norm(p[1] - p[0])
     bij = unit_vector(p[1] - p[0])
@@ -25,6 +33,12 @@ def w(p, R):
     wfik = s_f(ei.dot(bik))
 
     return wrij * wfij * wrik * wfik
+
+
+def f(p, R):
+    ei = R[:, 0]
+    bij = unit_vector(p[1] - p[0])
+    return ei.dot(bij)
 
 
 def s_r(x):
@@ -49,6 +63,12 @@ def e(i, j):
     return x
 
 
+def A(i):
+    x = np.zeros(3)
+    x[i] = 1.0
+    return cross_product_matrix(x)
+
+
 # ------------------------------------------------------------------
 # Configuration
 # ------------------------------------------------------------------
@@ -62,8 +82,11 @@ p = np.array([
     [0.5, 0.5, 0.5],
     [0.6, 0.7, -0.1]
 ])
-R = np.array([np.eye(3)])
+R = rotation_matrix_from_vector(np.random.uniform(-0.1, 0.1, 3))
+S = cross_product_matrix
+e1 = np.array([1.0, 0.0, 0.0])
 
+# --- position part --- #
 # --- compute derivatives numerically --- #
 h = 1e-9    # step
 dw1 = np.zeros((3, 3), dtype=np.float64)
@@ -73,10 +96,10 @@ for m in range(3):
 
 print(dw1)
 
-# # --- compute derivatives by closed formula --- #
+# --- compute derivatives by closed formula --- #
 dw2 = np.zeros((3, 3), dtype=np.float64)
 
-ei = R[0][:, 0]
+ei = R[:, 0]
 
 dij = np.linalg.norm(p[1] - p[0])
 bij = unit_vector(p[1] - p[0])
@@ -101,3 +124,41 @@ dw2[2] = - wfij*wfik*wrij*ds_r(dik) * bik + wrij*wrik*wfij*ds_f(nik) * Pik.dot(e
 print(dw2)
 
 print(np.allclose(dw1, dw2, atol=1e-4))
+
+# --- orientation part --- #
+# --- compute derivatives numerically --- #
+h = 1e-9    # step
+dw1 = np.zeros(3, dtype=np.float64)
+for m in range(3):
+    for d in range(3):
+        dw1[d] = ((w(p, R + h*A(d).dot(R)) - w(p, R - h*A(d).dot(R))) / (2*h)).item()
+
+print(dw1)
+
+# --- compute derivatives by closed formula --- #
+ei = R[:, 0]
+
+dij = np.linalg.norm(p[1] - p[0])
+bij = unit_vector(p[1] - p[0])
+# Pij = np.eye(3) - np.outer(bij, bij)
+dik = np.linalg.norm(p[2] - p[0])
+bik = unit_vector(p[2] - p[0])
+# Pik = np.eye(3) - np.outer(bik, bik)
+
+wrij = 1 - s_r(dij)
+wfij = s_f(ei.dot(bij))
+wrik = 1 - s_r(dik)
+wfik = s_f(ei.dot(bik))
+
+nij = ei.dot(bij)
+nik = ei.dot(bik)
+
+dw2 = 0.5 * wrij*wrik*S(ei).dot(wfik*ds_f(nij) * bij + wfij*ds_f(nik) * bik)
+
+# dw2 = cross_product_matrix_inverse(
+#     np.outer(bij, ei) - np.outer(ei, bij)
+# )
+
+print(dw2)
+
+print(np.allclose(dw1/2, dw2, atol=1e-4))
