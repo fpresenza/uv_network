ba236327452abce98072e1f09a07cca221b7ade2
fpresenza <francisco.presenza@gmail.com>
2026-03-30 17:34:34 -0300

    wip: simulate anl from data generated elsewhere

---
+++ tests/angles/anl/simu_decoupled_from_data.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+from dataclasses import dataclass
+import progressbar
+import numpy as np
+from transformations import unit_vector
+
+from uvnpy.toolkit.data import read_csv_numpy
+from uvnpy.graphs.core import edges_from_adjacency
+from uvnpy.dynamics.core import EulerIntegrator
+from uvnpy.angles.local_frame.core import angle_indices
+
+# ------------------------------------------------------------------
+# Functions, Classes and Configurations
+# ------------------------------------------------------------------
+np.set_printoptions(suppress=True, precision=10)
+
+
+@dataclass
+class Logs(object):
+    estimated_position: list
+    correction_u: list
+
+
+def extract_x(integrators):
+    return np.array([p.x() for p in integrators])
+
+
+def extract_u(integrators):
+    return np.array([p.u() for p in integrators])
+
+
+# ------------------------------------------------------------------
+# Simulation loop inner functions
+# ------------------------------------------------------------------
+
+
+def simu_step():
+    """Pose estimation algorithm"""
+    # --- data ---#
+    t = time[simu_counter]
+    p = position[simu_counter]
+    R = orientation[simu_counter]
+    edge_set = edges_from_adjacency(adjacency[simu_counter])
+    angle_set = angle_indices(nodes, edge_set).astype(int)
+    ub = control_u[simu_counter - 1]
+    wb = control_w[simu_counter - 1]
+
+    hatq = extract_x(hatq_int)
+
+    gradient_descent = np.zeros((n, 3), dtype=np.float64)
+
+    # --- similarity correction --- #
+    # measurements
+    qb = R[a].T.dot(p[b] - p[a])
+    qc = R[a].T.dot(p[c] - p[a])
+
+    # correction
+    k_s = 30.0
+    gradient_descent[a] -= k_s * hatq[a]
+    gradient_descent[b] -= k_s * (hatq[b] - qb)
+    gradient_descent[c] -= k_s * (hatq[c] - qc)
+
+    # # --- scale correction --- #
+    # # measurements
+    # dab2 = np.square(p[b] - p[a]).sum()
+    # dac2 = np.square(p[c] - p[a]).sum()
+
+    # # estimated values
+    # hat_dab2 = np.square(hatq[b] - hatq[a]).sum()
+    # hat_dac2 = np.square(hatq[c] - hatq[a]).sum()
+
+    # # correction
+    # k_s = 0.45
+    # scale_correction_ab = k_s * (hat_dab2 - dab2) * (hatq[a] - hatq[b])
+    # scale_correction_ac = k_s * (hat_dac2 - dac2) * (hatq[a] - hatq[c])
+    # gradient_descent[b] += scale_correction_ab
+    # gradient_descent[c] += scale_correction_ac
+    # gradient_descent[a] -= scale_correction_ab + scale_correction_ac
+
+    k_a = 1000.0
+    for i in nodes:
+        # --- angle correction --- #
+        out_neighbors = edge_set[:, 1][edge_set[:, 0] == i]
+
+        # estimated values
+        hat_distances = {
+            j: np.sqrt(np.square(hatq[j] - hatq[i]).sum()) for j in out_neighbors
+        }
+        hat_bearings = {j: unit_vector(hatq[j] - hatq[i]) for j in out_neighbors}
+
+        # measurements
+        bearings = {
+            j: R[i].T.dot(unit_vector(p[j] - p[i])) for j in out_neighbors
+        }
+
+        # correction
+        for j, k in angle_set[angle_set[:, 0] == i][:, 1:]:
+
+            dij = hat_distances[j]
+            bij = hat_bearings[j]
+            Pij = np.eye(3) - np.outer(bij, bij)
+
+            dik = hat_distances[k]
+            bik = hat_bearings[k]
+            Pik = np.eye(3) - np.outer(bik, bik)
+
+            # measured angles
+            aijk = bearings[j].dot(bearings[k])
+
+            eijk = bij.dot(bik) - aijk
+            nijk = Pij.dot(bik) / dij
+            nikj = Pik.dot(bij) / dik
+
+            angle_correction_j = k_a * eijk * nijk
+            angle_correction_k = k_a * eijk * nikj
+            gradient_descent[i] += angle_correction_j + angle_correction_k
+            gradient_descent[j] -= angle_correction_j
+            gradient_descent[k] -= angle_correction_k
+
+    for i in nodes:
+        # --- advance estimation --- #
+        hatq_int[i].step(t, - ub[a] - np.cross(wb[a], hatq[i]) + gradient_descent[i])
+        # hatq_int[i].step(t, gradient_descent[i])
+
+
+def log_step():
+    """Data log"""
+    logs.estimated_position.append(extract_x(hatq_int).ravel())
+    logs.correction_u.append(extract_u(hatq_int).ravel())
+
+
+# ------------------------------------------------------------------
+# Argument parse
+# ------------------------------------------------------------------
+parser = argparse.ArgumentParser(description='')
+parser.add_argument(
+    '-l', '--log_skip',
+    default=1, type=int, help='logger skip in number of simu_step_size'
+)
+arg = parser.parse_args()
+
+# ------------------------------------------------------------------
+# Configuration
+# ------------------------------------------------------------------
+# --- simulation parameters --- #
+log_skip = arg.log_skip
+
+# --- world parameters --- #
+time = read_csv_numpy('input_data/t.csv')
+log_num_steps = len(time)
+
+position = read_csv_numpy(
+    'input_data/position.csv'
+).reshape(log_num_steps, -1, 3)
+
+n = position.shape[1]
+nodes = np.arange(n)
+
+orientation = read_csv_numpy(
+    'input_data/orientation.csv'
+).reshape(log_num_steps, n, 3, 3)
+
+control_u = read_csv_numpy('input_data/control_u.csv').reshape(log_num_steps - 1, n, 3)
+control_w = read_csv_numpy('input_data/control_w.csv').reshape(log_num_steps - 1, n, 3)
+
+adjacency = read_csv_numpy(
+    'input_data/adjacency.csv'
+).reshape(log_num_steps, n, n)
+
+a, b, c = 0, 1, 2
+
+# refer initial position to body frame a
+position_a = (position[0] - position[0, a]).dot(orientation[0, a])
+
+est_position = np.random.normal(position_a, 2.0)
+hatq_int = [
+    EulerIntegrator(est_position[i])
+    for i in nodes
+]
+
+# ------------------------------------------------------------------
+# Simulation
+# ------------------------------------------------------------------
+# initialize logs
+logs = Logs(
+    estimated_position=[extract_x(hatq_int).ravel()],
+    correction_u=[extract_u(hatq_int).ravel()],
+)
+
+# run simulation
+simu_counter = 1
+bar = progressbar.ProgressBar(maxval=log_num_steps).start()
+
+while simu_counter < log_num_steps:
+
+    simu_step()
+    if (simu_counter % log_skip == 0):
+        log_step()
+
+    simu_counter += 1
+
+    bar.update(simu_counter)
+
+bar.finish()
+
+np.savetxt(
+    'simu_data/estimated_position.csv', logs.estimated_position, delimiter=','
+)
+np.savetxt('simu_data/correction_u.csv', logs.correction_u, delimiter=',')
---
+++ tests/angles/anl/time_plots_from_data.py
@@ -0,0 +1,417 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+
+from uvnpy.toolkit.data import read_csv_numpy
+from uvnpy.angles.local_frame.core import angle_function
+# from uvnpy.graphs.core import edges_from_adjacency
+from uvnpy.graphs.core import complete_edges
+
+plt.rcParams['text.usetex'] = False
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
+plt.rcParams['mathtext.fontset'] = 'dejavuserif'
+plt.rcParams['font.family'] = 'serif'
+
+# ------------------------------------------------------------------
+# Argument parse
+# ------------------------------------------------------------------
+parser = argparse.ArgumentParser(description='')
+parser.add_argument(
+    '-c', '--coupled',
+    default=False, action='store_true', help='Coupled or uncoupled estimation.'
+)
+arg = parser.parse_args()
+
+# ------------------------------------------------------------------
+# Read simulated data
+# ------------------------------------------------------------------
+t = read_csv_numpy('input_data/t.csv')
+log_num_steps = len(t)
+
+position = read_csv_numpy(
+    'input_data/position.csv'
+).reshape(log_num_steps, -1, 3)
+
+n = position.shape[1]
+nodes = np.arange(n)
+
+orientation = read_csv_numpy(
+    'input_data/orientation.csv'
+).reshape(log_num_steps, n, 3, 3)
+
+estimated_position = read_csv_numpy(
+    'simu_data/estimated_position.csv'
+).reshape(log_num_steps, n, 3)
+
+control_u = read_csv_numpy('input_data/control_u.csv').reshape(log_num_steps - 1, n, 3)
+control_w = read_csv_numpy('input_data/control_w.csv').reshape(log_num_steps - 1, n, 3)
+
+correction_u = read_csv_numpy('simu_data/correction_u.csv').reshape(log_num_steps, n, 3)
+
+if arg.coupled:
+    estimated_orientation = read_csv_numpy(
+        'simu_data/estimated_orientation.csv'
+    ).reshape(log_num_steps, n, 3, 3)
+    correction_w = read_csv_numpy(
+        'simu_data/correction_w.csv'
+    ).reshape(log_num_steps, n, 3)
+
+adjacency = read_csv_numpy(
+    'input_data/adjacency.csv'
+).reshape(log_num_steps, n, n)
+
+
+# edge_set = [edges_from_adjacency(adj) for adj in adjacency]
+edge_set = complete_edges(n)
+
+a = 0
+
+# position reconstruction
+rotated_position = np.matmul(
+    orientation[:, a], estimated_position.swapaxes(1, 2)
+).swapaxes(1, 2)
+reconstructed_position = position[:, np.newaxis, a] + rotated_position
+
+# ------------------------------------------------------------------
+# Plot position
+# ------------------------------------------------------------------
+fig, ax = plt.subplots(3, 1, figsize=(9.0, 6.0))
+fig.subplots_adjust(
+    bottom=0.215,
+    top=0.925,
+    wspace=0.33,
+    right=0.975,
+    left=0.18
+)
+
+for k, d in enumerate(['x', 'y', 'z']):
+    ax[k].tick_params(
+        axis='both',       # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        pad=1,
+        labelsize=9
+    )
+
+    ax[k].set_xlabel(r'$t\ (\mathrm{s})$', fontsize=10)
+    ax[k].set_ylabel(fr'$p_{{i, {d}}}, \hat{{p}}_{{i, {d}}} \ (\rm m)$', fontsize=10)
+    ax[k].grid(1)
+    # ax[k].set_ylim(-10.0, 50.0)
+
+    ax[k].plot(
+        t,
+        position[:, :, k],
+        lw=1.0,
+        ds='steps-post',
+    )
+    ax[k].plot(
+        t,
+        reconstructed_position[:, :, k],
+        lw=0.8,
+        color='0.5',
+        ls='--',
+        ds='steps-post',
+    )
+
+fig.savefig('time_plots/position.pdf', bbox_inches='tight')
+
+# ------------------------------------------------------------------
+# Plot position error
+# ------------------------------------------------------------------
+fig, ax = plt.subplots(3, 1, figsize=(9.0, 6.0))
+fig.subplots_adjust(
+    bottom=0.215,
+    top=0.925,
+    wspace=0.33,
+    right=0.975,
+    left=0.18
+)
+
+for k, d in enumerate(['x', 'y', 'z']):
+    ax[k].tick_params(
+        axis='both',       # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        pad=1,
+        labelsize=9
+    )
+
+    ax[k].set_xlabel(r'$t\ (\mathrm{s})$', fontsize=10)
+    ax[k].set_ylabel(fr'$\hat{{p}}_{{i, {d}}} - p_{{i, {d}}} \ (\rm m)$', fontsize=10)
+    ax[k].grid(1)
+
+    ax[k].plot(
+        t,
+        reconstructed_position[:, :, k] - position[:, :, k],
+        lw=1.0,
+        ds='steps-post'
+    )
+
+fig.savefig('time_plots/position_error.pdf', bbox_inches='tight')
+
+fig, ax = plt.subplots(figsize=(9.0, 3.0))
+fig.tight_layout()
+
+ax.tick_params(
+    axis='both',       # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    pad=1,
+    labelsize=15
+)
+
+ax.set_xlabel(r'$t\ (\mathrm{s})$', fontsize=15)
+ax.set_ylabel(r'$\| \hat{{p}}_{i} - p_{i} \| \ (\rm m)$', fontsize=15)
+ax.grid(1)
+
+ax.plot(
+    t,
+    np.sqrt(np.square(reconstructed_position - position).sum(axis=-1)),
+    lw=2.0,
+    ds='steps-post'
+)
+
+fig.savefig('time_plots/position_error_agg.pdf', bbox_inches='tight')
+
+# ------------------------------------------------------------------
+# Plot orientation error
+# ------------------------------------------------------------------
+# position reconstruction
+# Q = orientation
+# hatR = np.matmul(estimated_orientation[:, a, np.newaxis], estimated_orientation)
+
+if arg.coupled:
+    Q = np.matmul(orientation[:, a, np.newaxis].swapaxes(2, 3), orientation)
+    hatQ = estimated_orientation
+
+    E = np.matmul(Q.swapaxes(2, 3), hatQ)
+    e = 0.5 * (3 - np.trace(E, axis1=2, axis2=3))
+
+    fig, ax = plt.subplots(figsize=(9.0, 6.0))
+    fig.subplots_adjust(
+        bottom=0.215,
+        top=0.925,
+        wspace=0.33,
+        right=0.975,
+        left=0.18
+    )
+
+    ax.tick_params(
+        axis='both',       # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        pad=1,
+        labelsize=9
+    )
+
+    ax.set_xlabel(r'$t\ (\mathrm{s})$', fontsize=10)
+    ax.set_ylabel(
+        r'$\frac{1}{2} \  \mathrm{tr}\left(I - Q_i^\top \hat{Q}_{i}\right) \ (\rm m)$',
+        fontsize=10
+    )
+    ax.grid(1)
+
+    ax.plot(
+        t,
+        e,
+        lw=1.0,
+        ds='steps-post'
+    )
+
+    fig.savefig('time_plots/orientation_error.pdf', bbox_inches='tight')
+
+# ------------------------------------------------------------------
+# Plot control
+# ------------------------------------------------------------------
+fig, ax = plt.subplots(3, 2, figsize=(18.0, 6.0))
+fig.subplots_adjust(
+    bottom=0.215,
+    top=0.925,
+    wspace=0.33,
+    right=0.975,
+    left=0.18
+)
+fig.tight_layout()
+
+for k, d in enumerate(['x', 'y', 'z']):
+    ax[k, 0].tick_params(
+        axis='both',       # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        pad=1,
+        labelsize=9
+    )
+
+    ax[k, 0].set_xlabel(r'$t\ (\mathrm{s})$', fontsize=10)
+    ax[k, 0].set_ylabel(fr'$u_{{i, {d}}} \ (\rm m / s)$', fontsize=10)
+    ax[k, 0].set_ylim(-2.0, 2.0)
+    ax[k, 0].grid(1)
+
+    ax[k, 0].plot(t[1:], control_u[:, :, k], lw=1.0, ds='steps-post')
+
+    ax[k, 1].tick_params(
+        axis='both',       # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        pad=1,
+        labelsize=9
+    )
+
+    ax[k, 1].set_xlabel(r'$t\ (\mathrm{s})$', fontsize=10)
+    ax[k, 1].set_ylabel(fr'$w^i_{{i, {d}}} \ (\rm m / s)$', fontsize=10)
+    ax[k, 1].set_ylim(-2.0, 2.0)
+    ax[k, 1].grid(1)
+
+    ax[k, 1].plot(t[1:], control_w[:, :, k], lw=1.0, ds='steps-post')
+
+fig.savefig('time_plots/control.pdf', bbox_inches='tight')
+
+# ------------------------------------------------------------------
+# Plot correction
+# ------------------------------------------------------------------
+if arg.coupled:
+    fig, ax = plt.subplots(3, 2, figsize=(18.0, 6.0))
+    fig.subplots_adjust(
+        bottom=0.215,
+        top=0.925,
+        wspace=0.33,
+        right=0.975,
+        left=0.18
+    )
+
+    for k, d in enumerate(['x', 'y', 'z']):
+        ax[k, 0].tick_params(
+            axis='both',       # changes apply to the x-axis
+            which='both',      # both major and minor ticks are affected
+            pad=1,
+            labelsize=9
+        )
+
+        ax[k, 0].set_xlabel(r'$t\ (\mathrm{s})$', fontsize=10)
+        ax[k, 0].set_ylabel(
+            fr'$\delta \hat{{p}}_{{i, {d}}} \ (\rm m / s)$', fontsize=10
+        )
+        # ax[k, 0].set_ylim(-1e-4, 1e-4)
+        ax[k, 0].grid(1)
+
+        ax[k, 0].plot(t, correction_u[:, :, k], lw=1.0, ds='steps-post')
+
+        ax[k, 1].tick_params(
+            axis='both',       # changes apply to the x-axis
+            which='both',      # both major and minor ticks are affected
+            pad=1,
+            labelsize=9
+        )
+
+        ax[k, 1].set_xlabel(r'$t\ (\mathrm{s})$', fontsize=10)
+        ax[k, 1].set_ylabel(
+            fr'$\delta \hat{{Q}}_{{i, {d}}} \ (\rm m / s)$', fontsize=10
+        )
+        # ax[k, 1].set_ylim(-1e-4, 1e-4)
+        ax[k, 1].grid(1)
+
+        ax[k, 1].plot(t, correction_w[:, :, k], lw=1.0, ds='steps-post')
+
+    fig.savefig('time_plots/correction.pdf', bbox_inches='tight')
+
+else:
+
+    fig, ax = plt.subplots(3, figsize=(9.0, 6.0))
+    fig.subplots_adjust(
+        bottom=0.215,
+        top=0.925,
+        wspace=0.33,
+        right=0.975,
+        left=0.18
+    )
+
+    for k, d in enumerate(['x', 'y', 'z']):
+        ax[k].tick_params(
+            axis='both',       # changes apply to the x-axis
+            which='both',      # both major and minor ticks are affected
+            pad=1,
+            labelsize=9
+        )
+
+        ax[k].set_xlabel(r'$t\ (\mathrm{s})$', fontsize=10)
+        ax[k].set_ylabel(fr'$\delta \hat{{p}}_{{i, {d}}} \ (\rm m / s)$', fontsize=10)
+        # ax[k].set_ylim(-1e-4, 1e-4)
+        ax[k].grid(1)
+
+        ax[k].plot(t, correction_u[:, :, k], lw=1.0, ds='steps-post')
+
+    fig.savefig('time_plots/correction.pdf', bbox_inches='tight')
+# ------------------------------------------------------------------
+# Plot angle error
+# ------------------------------------------------------------------
+fig, ax = plt.subplots(figsize=(9.0, 6.0))
+fig.subplots_adjust(
+    bottom=0.215,
+    top=0.925,
+    wspace=0.33,
+    right=0.975,
+    left=0.18
+)
+
+ax.tick_params(
+    axis='both',       # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    pad=1,
+    labelsize=9
+)
+
+ax.set_xlabel(r'$t\ (\mathrm{s})$', fontsize=10)
+ax.set_ylabel(r'$|\hat{a}_{ijk} - a_{ijk}|$', fontsize=10)
+ax.grid(1)
+
+ax.plot(
+    t,
+    [
+        np.square(angle_function(edge_set, hatp) - angle_function(edge_set, p))
+        for hatp, p in zip(estimated_position, position)
+    ],
+    lw=1.0, ds='steps-post'
+)
+fig.savefig('time_plots/angle_error.pdf', bbox_inches='tight')
+
+# ------------------------------------------------------------------
+# Plot distance error
+# ------------------------------------------------------------------
+distance = np.linalg.norm(
+    position[:, np.newaxis, :, :] - position[:, :, np.newaxis, :],
+    axis=-1
+)
+estimated_distance = np.linalg.norm(
+    estimated_position[:, np.newaxis, :, :] - estimated_position[:, :, np.newaxis, :],
+    axis=-1
+)
+
+fig, ax = plt.subplots(figsize=(9.0, 6.0))
+fig.subplots_adjust(
+    bottom=0.215,
+    top=0.925,
+    wspace=0.33,
+    right=0.975,
+    left=0.18
+)
+
+ax.tick_params(
+    axis='both',       # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    pad=1,
+    labelsize=9
+)
+
+ax.set_xlabel(r'$t\ (\mathrm{s})$', fontsize=10)
+ax.set_ylabel(r'$|\hat{d}_{ij} - d_{ij}|$', fontsize=10)
+ax.grid(1)
+
+ax.plot(
+    t,
+    np.unique(
+        np.abs(estimated_distance - distance).reshape(log_num_steps, -1), axis=-1
+    )[:, 1:],
+    lw=1.0,
+    ds='steps-post'
+)
+fig.savefig('time_plots/distance_error.pdf', bbox_inches='tight')
+
+plt.show()
